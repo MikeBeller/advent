d051c4b81466c52bb9b04a03d9269ff16dc85d47
Mike Beller <bellermike@gmail.com>
2019-11-15 16:03:42 -0500

    20180302

---
+++ 2018/03/20180302.py
@@ -0,0 +1,33 @@
+import sys
+import numpy as np
+
+def get_data():
+    r = []
+    for line in sys.stdin:
+        f = line.strip().split()
+        iid = int(f[0][1:])
+        l,t = [int(x) for x in f[2].replace(':','').split(",")]
+        w,h = [int(x) for x in f[3].split('x')]
+        r.append(dict(iid=iid, left=l, top=t, width=w, height=h))
+    return r
+
+def blit(a, l, t, w, h):
+    a[l:(l+w),t:(t+h)] += 1
+
+def check(a, l, t, w, h):
+    return (a[l:(l+w),t:(t+h)] == 1).all()
+
+def main():
+    data = get_data()
+    max_x = max(d['left'] + d['width'] for d in data)
+    max_y = max(d['top'] + d['height'] for d in data)
+    a = np.zeros((max_x+1,max_y+1),dtype=np.int32)
+    for d in data:
+        blit(a, d['left'], d['top'], d['width'], d['height'])
+    for d in data:
+        if check(a, d['left'], d['top'], d['width'], d['height']):
+            print(d['iid'])
+
+if __name__ == '__main__':
+    main()
+
