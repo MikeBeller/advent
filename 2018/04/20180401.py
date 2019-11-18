1c96eb3a981098fc2c48929465c00da8ad45ca25
Mike Beller <bellermike@gmail.com>
2019-11-18 17:10:30 -0500

    Working on an approach

--- 2018/04/20180401.py
+++ 2018/04/20180401.py
@@ -1,17 +1,21 @@
 import sys
 import numpy as np
+from datetime import datetime, timedelta
 
 def read_input():
     data = []
     for line in sorted(sys.stdin):
         f = line.strip().split()
         d = dict(date=f[0][1:], time=f[1][:-1])
+        dts = f[0][1:] + ' ' + f[1][:-1]
+        dt = datetime.fromisoformat(dts)
+        d = dict(dt=dt)
         if f[2] == "Guard":
             d['cmd'] = 'start'
             d['gid'] = f[3]
         elif f[2] == 'falls':
             d['cmd'] = 'sleep'
-        elif v[3] == 'wakes':
+        elif f[2] == 'wakes':
             d['cmd'] = 'wake'
         else:
             assert False
@@ -21,5 +25,19 @@ def read_input():
 
 def main():
     data = read_input()
+    for d in data:
+        if d['cmd'] == 'start':
+            t = d['dt']
 
+            # advance to start of shift
+            while t.hour() == 23:
+                t += timedelta(minutes=1)
+
+            while t.hour() == 0:
+                pass
+
+
+
+if __name__ == '__main__':
+    main()
 
