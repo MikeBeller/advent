1585968fb5ec39380afca520bc5ff1d619a1363c
Mike Beller <bellermike@gmail.com>
2019-11-30 16:41:33 -0500

    Non working version of 201807 part 2

--- 2018/07/201807_go.go
+++ 2018/07/201807_go.go
@@ -2,14 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
 	"sort"
+	"strconv"
 	"strings"
 )
 
 type Task struct {
-	Name     string
-	Requires []*Task
-	Done     bool
+	Name           string
+	Requires       []*Task
+	Done           bool
+	CompletionTime int
 }
 
 func readData() (data [][2]string) {
@@ -24,32 +28,7 @@ func readData() (data [][2]string) {
 	return
 }
 
-func notDone(ts []string, done map[string]bool) (nd []string) {
-	for _, t := range ts {
-		if !done[t] {
-			nd = append(nd, t)
-		}
-	}
-	return
-}
-
-func main() {
-	data := readData()
-	tasks := make(map[string]*Task)
-	for _, p := range data {
-		t0, ok := tasks[p[0]]
-		if !ok {
-			t0 = &Task{Name: p[0]}
-			tasks[p[0]] = t0
-		}
-		t1, ok := tasks[p[1]]
-		if !ok {
-			t1 = &Task{Name: p[1]}
-			tasks[p[1]] = t1
-		}
-		t1.Requires = append(t1.Requires, t0)
-	}
-
+func partOne(tasks map[string]*Task) []string {
 	ans := []string{}
 	for {
 		ts := []string{}
@@ -77,5 +56,112 @@ func main() {
 		tasks[ts[0]].Done = true
 	}
 
-	fmt.Println(strings.Join(ans, ""))
+	return ans
+}
+
+func aToI(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal("invalid integer", s)
+	}
+	return i
+}
+
+func taskDuration(n string, baseTime int) int {
+	return int(n[0]) - 64 + baseTime
+}
+
+/*
+func maxTaskCompletion(n string, tasks map[string]*Task) int {
+	mx := -1
+	for _, t := range tasks[n].Requires {
+		if t.CompletionTime > mx {
+			mx = t.CompletionTime
+		}
+	}
+	return mx
+}
+
+type Sim struct {
+	BaseTime int
+	Time     int
+	Workers  []*Task
+	Queue    []*Task
+	Tasks    map[string]*Task
+}
+
+func (s *Sim) processTask(n string) {
+	task := tasks[n]
+	if sim.hasUnsatisfiedDependencies(task) {
+		s.advance()
+	}
+}
+
+func NewSim(baseTime, numWorkers int, tasks map[string]*Task) *Sim {
+	return &Sim{BaseTime: baseTime, Workers: make([]*Task, numWorkers), Tasks: tasks}
+}
+*/
+
+func partTwo(tasks map[string]*Task, baseTime int, numWorkers int) int {
+	tm := 0
+	for {
+		ready := []string{}
+		for _, task := range tasks {
+			if task.Done {
+				continue
+			}
+			nnd := 0
+			for _, tt := range task.Requires {
+				if !tt.Done {
+					nnd++
+				}
+			}
+			if nnd == 0 {
+				ready = append(ready, task.Name)
+			}
+		}
+		if len(ready) == 0 {
+			break
+		}
+
+		sort.StringSlice(ready).Sort()
+		for _, n := range ready {
+			tasks[n].CompletionTime = tm + taskDuration(n, baseTime)
+		}
+		sort.Slice(ready, func(i, j int) bool { return tasks[ready[i]].CompletionTime < tasks[ready[j]].CompletionTime })
+		tasks[ready[0]].Done = true
+		tm = tasks[ready[0]].CompletionTime
+	}
+	return tm
+}
+
+func main() {
+	data := readData()
+	tasks := make(map[string]*Task)
+	for _, p := range data {
+		t0, ok := tasks[p[0]]
+		if !ok {
+			t0 = &Task{Name: p[0]}
+			tasks[p[0]] = t0
+		}
+		t1, ok := tasks[p[1]]
+		if !ok {
+			t1 = &Task{Name: p[1]}
+			tasks[p[1]] = t1
+		}
+		t1.Requires = append(t1.Requires, t0)
+	}
+
+	taskOrder := partOne(tasks)
+	fmt.Println("PartOne:", strings.Join(taskOrder, ""))
+
+	for _, t := range tasks {
+		t.Done = false
+	}
+
+	baseTime := aToI(os.Args[1])
+	numWorkers := aToI(os.Args[2])
+
+	time := partTwo(tasks, baseTime, numWorkers)
+	fmt.Println("PartTwo:", time)
 }
