ae9df3997459264133100ff85834b8cb5c4d3377
Mike Beller <bellermike@gmail.com>
2019-11-30 18:34:10 -0500

    Working version of 201808 first part

--- 2018/08/201808_go.go
+++ 2018/08/201808_go.go
@@ -1,16 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Node struct {
+	Meta     []int
+	Children []*Node
+}
+
+func readNum() int {
+	var c int
+	n, err := fmt.Scan(&c)
+	if n != 1 || err != nil {
+		panic("WTF?")
+	}
+	return c
+}
+
+func readTree() *Node {
+	node := &Node{}
+	nNodes := readNum()
+	nMeta := readNum()
+	for i := 0; i < nNodes; i++ {
+		child := readTree()
+		node.Children = append(node.Children, child)
+	}
+
+	for i := 0; i < nMeta; i++ {
+		node.Meta = append(node.Meta, readNum())
+	}
+
+	return node
+
+}
+
+func sumMeta(t *Node) int {
+	sm := 0
+	for _, m := range t.Meta {
+		sm += m
+	}
+	for _, c := range t.Children {
+		sm += sumMeta(c)
+	}
+	return sm
+}
 
 func main() {
-	r := []int{}
-	for {
-		var c int
-		n, err := fmt.Scan(&c)
-		if n != 1 || err != nil {
-			break
-		}
-		r = append(r, c)
-	}
-	fmt.Println(r)
+	t := readTree()
+	m := sumMeta(t)
+	fmt.Println(m)
 }
