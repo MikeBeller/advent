95a6cbe73665964b344bf7939956e19814b7c5ef
Mike Beller <bellermike@gmail.com>
2019-11-30 12:16:02 -0500

    Working version of 201807 part 1

--- 2018/07/201807_go.go
+++ 2018/07/201807_go.go
@@ -2,7 +2,15 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strings"
 )
+
+type Task struct {
+	Name     string
+	Requires []*Task
+	Done     bool
+}
 
 func readData() (data [][2]string) {
 	var x, y string
@@ -16,12 +24,58 @@ func readData() (data [][2]string) {
 	return
 }
 
+func notDone(ts []string, done map[string]bool) (nd []string) {
+	for _, t := range ts {
+		if !done[t] {
+			nd = append(nd, t)
+		}
+	}
+	return
+}
+
 func main() {
 	data := readData()
-	requires := make(map[string][]string)
+	tasks := make(map[string]*Task)
 	for _, p := range data {
-		requires[p[1]] = append(requires[p[1]], p[0])
+		t0, ok := tasks[p[0]]
+		if !ok {
+			t0 = &Task{Name: p[0]}
+			tasks[p[0]] = t0
+		}
+		t1, ok := tasks[p[1]]
+		if !ok {
+			t1 = &Task{Name: p[1]}
+			tasks[p[1]] = t1
+		}
+		t1.Requires = append(t1.Requires, t0)
 	}
 
-	fmt.Println("vim-go", requires)
+	ans := []string{}
+	for {
+		ts := []string{}
+		for _, task := range tasks {
+			if task.Done {
+				continue
+			}
+			nnd := 0
+			for _, tt := range task.Requires {
+				if !tt.Done {
+					nnd++
+				}
+			}
+			if nnd == 0 {
+				ts = append(ts, task.Name)
+			}
+		}
+		if len(ts) == 0 {
+			break
+		}
+
+		// Only add the min (could write min loop but i'm lazy)
+		sort.StringSlice(ts).Sort()
+		ans = append(ans, ts[0])
+		tasks[ts[0]].Done = true
+	}
+
+	fmt.Println(strings.Join(ans, ""))
 }
