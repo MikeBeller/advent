daeec8db542f47bdbff4752bfbc99eb1b46812a4
Mike Beller <bellermike@gmail.com>
2019-12-06 10:56:41 -0500

    Still working on numpy version of 201904

--- 2019/04/201904_np.py
+++ 2019/04/201904_np.py
@@ -2,23 +2,18 @@ import numpy as np
 
 tens = 10 ** np.arange(5, -1, -1)
 #ns = np.c_[[ 111111, 223450, 123789,] ]
-#ns = np.c_[[ 112233, 123444, 111122,] ]
-ns = np.c_[np.arange(136818, 685980)]
+ns = np.c_[[ 112233, 123444, 111122,] ]
+#ns = np.c_[np.arange(136818, 685980)]
 
 ds = ns // tens % 10
-df = np.diff(ds, axis=1)
 df = ds[:,1:]-ds[:,:-1]
 nondecreasing = np.all(df > -1, axis=1)
-ns = ns[nondecreasing]
-df = df[nondecreasing]
+dsn = ds[nondecreasing]
 
+print(dsn)
+x = dsn[:,1:] != dsn[:,:-1]
+#y = np.tile(np.arange(1,6), (len(dsn), 1)) * x
+y = np.arange(1,6) * x
 
-repeating = np.any(df == 0, axis=1)
-ns = ns[repeating]
-df = df[repeating]
-print("Part1", len(ns))
-
-zs = np.where(df == 0, 0, 1)
-morethantwo = np.any(np.diff(zs) == 0, axis=1)
-ns = ns[morethantwo == False]
-print("Part2", len(ns))
+print(x)
+print(y)
