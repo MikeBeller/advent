b20279dba2bfe18f4d0761c334d729600710c1f0
Mike Beller <bellermike@gmail.com>
2019-12-05 21:57:06 -0500

    Numpy solution -- part1 is working, not part2

---
+++ 2019/04/201904_np.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+tens = 10 ** np.arange(5, -1, -1)
+#ns = np.c_[[ 111111, 223450, 123789,] ]
+#ns = np.c_[[ 112233, 123444, 111122,] ]
+ns = np.c_[np.arange(136818, 685980)]
+
+#print(ns)
+ds = ns // tens % 10
+
+#print(ds)
+
+df = np.diff(ds, axis=1)
+#print(df)
+
+nondecreasing = np.all(df > -1, axis=1)
+#print(nondecreasing)
+
+ns = ns[nondecreasing]
+df = df[nondecreasing]
+
+repeating = np.any(df == 0, axis=1)
+ns = ns[repeating]
+df = df[repeating]
+print("Part1", len(ns))
+
+zs = np.where(df == 0, 0, 1)
+morethantwo = np.any(np.diff(zs) == 0, axis=1)
+ns = ns[morethantwo == False]
+print("Part2", len(ns))
