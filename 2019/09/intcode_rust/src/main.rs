e02c044632ab993a4d53f76ac41f991d9427914b
MikeBeller <bellermike@gmail.com>
2019-12-10 02:56:56 +0000

    Rust version.  Release version is 30 times faster than python, and 10 times faster than pypy3

--- 2019/09/intcode_rust/src/main.rs
+++ 2019/09/intcode_rust/src/main.rs
@@ -1,3 +1,4 @@
+use std::fs;
 
 struct Intcode {
     memory: Vec<i64>,
@@ -8,18 +9,22 @@ struct Intcode {
 }
 
 impl Intcode {
-    fn new(prog: &Vec<i64>, inp: &Vec<i64>) -> Intcode {
+    fn new(prog: &[i64], inp: &[i64]) -> Intcode {
+        let mut input = inp.to_vec();
+        input.reverse();
         Intcode{
-            memory: prog.clone(),
-            input: inp.clone(),
+            memory: prog.to_vec(),
+            input: input,
             output: vec![],
             pc: 0,
             relative_base: 0
         }
     }
 
+    // Resize memory to be big enough to include ad, which
+    // means one greater than ad itself.
     fn resize_memory(&mut self, ad: usize) {
-        if ad > self.memory.len() {
+        if ad+1 > self.memory.len() {
             self.memory.resize(ad+1, 0);
         }
     }
@@ -75,8 +80,68 @@ impl Intcode {
             .map(|i| (o/i) % 10)
             .collect::<Vec<i64>>();
 
-        99
+        match op {
+            1 | 2 => {
+                let a = self.get_parameter(self.pc+1, m[0]);
+                let b = self.get_parameter(self.pc+2, m[1]);
+                self.set_parameter(self.pc+3, m[2],
+                                   if op == 1 { a + b } else { a * b });
+                self.pc += 4;
+            },
+
+            3 => {
+                let n = self.input.pop().unwrap();
+                self.set_parameter(self.pc+1, m[0], n);
+                self.pc += 2;
+            },
+
+            4 => {
+                let n = self.get_parameter(self.pc+1, m[0]);
+                self.output.push(n);
+                self.pc += 2;
+            },
+
+            5 | 6 => {
+                let a = self.get_parameter(self.pc+1, m[0]);
+                let b = self.get_parameter(self.pc+2, m[1]);
+                if (op == 5 && a != 0) || (op == 6 && a == 0) {
+                    self.pc = b;
+                } else {
+                    self.pc += 3;
+                }
+            },
+
+            7 | 8 => {
+                let a = self.get_parameter(self.pc+1, m[0]);
+                let b = self.get_parameter(self.pc+2, m[1]);
+                if (op == 7 && a < b) || (op == 8 && a == b) {
+                    self.set_parameter(self.pc+3, m[2], 1);
+                } else {
+                    self.set_parameter(self.pc+3, m[2], 0);
+                }
+                self.pc += 4;
+            },
+
+            9 => {
+                let a = self.get_parameter(self.pc+1, m[0]);
+                self.relative_base += a;
+                self.pc += 2;
+            },
+
+            _ => {panic!("invalid opcode {}", op);},
+        }
+
+        op
     }
+}
+
+fn runprog(prgstr: &str, inp: &[i64]) -> Vec<i64> {
+    let prog = prgstr.split(',')
+        .map(|s| s.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+    let mut ic = Intcode::new(&prog, inp);
+    ic.run();
+    ic.output
 }
 
 #[test]
@@ -103,4 +168,31 @@ fn test_param() {
     assert_eq!(ic.memory[1], 22);
     ic.set_parameter(2, 2, 88);
     assert_eq!(ic.memory[3], 88);
+}
+
+#[test]
+fn test_run() {
+    assert!(runprog("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", &[0]) == [0]);
+    assert!(runprog("3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9", &[1]) == [1]);
+
+    assert!(runprog("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", &[-11]) == [999]);
+    assert!(runprog("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", &[8]) == [1000]);
+    assert!(runprog("3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99", &[999]) == [1001]);
+
+    assert!(runprog("1102,34915192,34915192,7,4,7,99,0", &[]) == [1219070632396864]);
+    assert!(runprog("104,1125899906842624,99", &[]) == [1125899906842624]);
+    //assert!(runprog("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99", &[]) == [109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99]);
+    println!("{:?}", runprog("109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99", &[]));
+
+}
+
+fn main() {
+    let instr = fs::read_to_string("../input.txt").unwrap();
+    let progstr = instr.trim_end();
+    let ans1 = runprog(progstr, &[1]);
+    println!("{:?}", ans1);
+
+    let ans2 = runprog(progstr, &[2]);
+    println!("{:?}", ans2);
+
 }
