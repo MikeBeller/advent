b7bef6459e1b267804baca2e23c37e06a5422963
MikeBeller <bellermike@gmail.com>
2019-12-13 17:39:50 -0500

    Working version of day 13 both parts!

--- 2019/13/201913.py
+++ 2019/13/201913.py
@@ -1,6 +1,4 @@
 import os
-import time
-import numpy as np
 from typing import Dict, List
 from intcode import Intcode
 from collections import namedtuple
@@ -22,13 +20,6 @@ def part_one(prog: List[int]) -> int:
     print(min(p.x for p in grid.keys()), max(p.x for p in grid.keys()))
     print(min(p.y for p in grid.keys()), max(p.y for p in grid.keys()))
     return sum(1 if v == 2 else 0 for v in grid.values())
-
-def print_grid(g: np.ndarray) -> None:
-    p = " #X_@"
-    for r in range(20):
-        #for c in range(40):
-        #    print(p[g[r,c]], sep='', end='')
-        print("".join(p[g[r,c]] for c in range(40)))
 
 def rungame(scr, prog: List[int]) -> int:
     scr.nodelay(True)
@@ -54,17 +45,19 @@ def rungame(scr, prog: List[int]) -> int:
         x,y,tid,*ic.output = ic.output
         if x == -1 and y == 0:
             score = tid
+            scr.addstr(0, 0, str(score))
             game_is_live = True
         else:
             #grid[y,x] = tid
-            scr.addstr(y, x, " #X_@"[tid])
+            scr.addch(y, x, " #X_@"[tid])
         if tid == 3:
             paddle = Point(x,y)
         elif tid == 4:
             if ball != Point(-1,-1):
                 dx = x - ball.x
             ball = Point(x,y)
-        if ball != Point(-1,-1) and paddle != Point(-1,-1):
+        if game_is_live:
+            # interactive game!
             #c = scr.getch()
             #if c == 260:
             #    paddle_dir = -1
@@ -73,15 +66,22 @@ def rungame(scr, prog: List[int]) -> int:
             #else:
             #    paddle_dir = 0
 
-            if dx < 0:
-                paddle_dir = -1
-            elif dx > 0:
-                paddle_dir = 1
+            # heuristic which seems to work!
+            if ball.x < paddle.x:
+                if dx < 0:
+                    paddle_dir = -1
+                else:
+                    paddle_dir = 0
+            elif ball.x > paddle.x:
+                if dx > 0:
+                    paddle_dir = 1
+                else:
+                    paddle_dir = 0
             else:
                 paddle_dir = 0
 
-        if game_is_live:
-            time.sleep(0.3)
+        #if game_is_live:
+        #    time.sleep(0.05)
         scr.refresh()
 
     return score
