b1d7c4ecff4bfd39ae156320d2db3f9093ad81cb
MikeBeller <bellermike@gmail.com>
2019-12-24 17:50:25 -0500

    Passes first test.  But pure greedy algo doesn't work for second test.

--- 2019/18/201918.py
+++ 2019/18/201918.py
@@ -9,17 +9,6 @@ class Node:
         self.loc = Point(x, y)
         self.mark = mark
         self.conns: Set[Node] = set()
-        self.visited = False
-
-test1 = """#########
-#b.A.@.a#
-#########"""
-
-test2 = """########################
-#f.D.E.e.C.b.A.@.a.B.c.#
-######################.#
-#d.....................#
-########################"""
 
 def move(p: Point, d: int) -> Point:
     if d == 0:
@@ -52,11 +41,76 @@ def read_data(s: str) -> Tuple[Dict[Point,Node], Dict[str,Point]]:
                 gr[q].conns.add(gr[p])
     return gr, loc
 
-def part_one(dstr: str) -> None:
+all_keys = set('abcdefghijklmnopqrstuvwxyz')
+all_doors = set('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+def dist_to_stuff_from(gr: Dict[Point,Node], st: Point, keys: Dict[str,bool], doors: Dict[str,bool]) -> Dict[str,int]:
+    ds: Dict[str,int] = {}
+    vs: Dict[Node,bool] = {}
+
+    def bfs(nd: Node, depth: int) -> None:
+        if nd in vs:  # stop because you already visited this node
+            return
+        vs[nd] = True
+        if nd.mark in doors and not doors[nd.mark]:
+            ds[nd.mark] = depth
+            return  # stop this path because you hit a closed door
+        if nd.mark in keys and not keys[nd.mark]:
+            ds[nd.mark] = depth
+        for cn in nd.conns:
+            bfs(cn, depth+1)
+
+    bfs(gr[st], 0)
+    return ds
+
+def part_one(dstr: str) -> int:
     gr,loc = read_data(dstr)
     print(gr)
     print(loc)
 
-part_one(test1)
+    # one entry per key on the map, true only if you have the key
+    keys = dict((k,False) for k in loc.keys() if k in "abcdefghijklmnopqrstuvwxyz")
+    # one entry per door on the map, true only if door is unlocked
+    doors = dict((d,False) for d in loc.keys() if d in "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+    total_dist = 0
+    pos = loc['@']
+    while not all(keys.values()):  # while you don't have all keys
+        ds = dist_to_stuff_from(gr, pos, keys, doors)
+        candidates: List[Tuple[str,int]] = []
+        for name,dist in ds.items():
+            if name in keys:
+                if not keys[name]: # don't have the key yet
+                    candidates.append((name,dist))
+            if name in doors:
+                if not doors[name] and keys[name.lower()]:
+                    candidates.append((name,dist))
+        best_name,best_dist = min(candidates, key=lambda p: p[1])
+        print("CANDIDATES", candidates, "BEST", best_name, best_dist)
+        pos = loc[best_name]
+        total_dist += best_dist
+        if best_name in doors:
+            assert best_name.lower() in keys
+            doors[best_name] = True  # unlock the door
+        elif best_name in keys:
+            keys[best_name] = True
+        else:
+            assert False, "WTF?"
+    print("TOTAL DIST", total_dist)
+    return total_dist
+
+test1 = """#########
+#b.A.@.a#
+#########"""
+
+assert part_one(test1) == 8
+
+test2 = """########################
+#f.D.E.e.C.b.A.@.a.B.c.#
+######################.#
+#d.....................#
+########################"""
+
+print(part_one(test2))
 
 
