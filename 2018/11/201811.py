09fa3575c363bcbbdac1a29535d6a792ee9b0fbe
Mike Beller <bellermike@gmail.com>
2020-02-27 12:47:00 -0500

    Working version of 201911 in python

--- 2018/11/201811.py
+++ 2018/11/201811.py
@@ -1,4 +1,5 @@
 import numpy as np
+from typing import Tuple
 
 SIZE = 300  # size of the total playing field
 
@@ -14,11 +15,15 @@ def power_level(sn: int) -> np.ndarray:
 assert power_level(8)[4,2] == 4
 assert power_level(57)[78,121] == -5
 
+num_ops = 0
+
 def filter_pl(m: np.ndarray, sq: int) -> np.ndarray:
-    a = np.zeros((SIZE-sq, SIZE-sq), dtype=np.int64)
-    for r in range(SIZE - sq):
-        for c in range(SIZE - sq):
+    global num_ops
+    a = np.zeros((SIZE-sq + 1, SIZE-sq + 1), dtype=np.int64)
+    for r in range(SIZE - sq + 1):
+        for c in range(SIZE - sq + 1):
             a[c,r] = np.sum(m[c:c+sq,r:r+sq])
+            num_ops += sq * sq
     return a
 
 def part1(sn: int):
@@ -30,9 +35,29 @@ def part1(sn: int):
 
 assert part1(18) == (33,45)
 
+def filter_size(pl: np.ndarray, sz: int) -> Tuple[int,Tuple[int,int], int]:
+    a = filter_pl(pl, sz)
+    ind1d = np.argmax(a)
+    ind = np.unravel_index(ind1d, a.shape)
+    return (a[ind], ind, sz)
+
+def part2(sn: int) -> Tuple[Tuple[int,int], int]:
+    pl = power_level(sn)
+    rs = [filter_size(pl, sz) for sz in range(1,SIZE+1)]
+    (mxv,mxind,mxsz) = max(rs, key=lambda r: r[0])
+    return (mxind[1]+1, mxind[0]+1), mxsz
+
+#assert part2(18) == ((269, 90), 16)
+
 def main():
     ans1 = part1(3999)
     print("PART1:", ans1)
 
-main()
+    ans2 = part2(3999)
+    #ans2 = part2(18)
+    print("PART2:", ans2)
+    print("NUM OPS:", num_ops)
+
+if __name__ == '__main__':
+    main()
 
