bfd2743efc59f775bdf6a5cc60e6be0632e4d25d
Mike Beller <bellermike@gmail.com>
2020-02-26 20:19:12 -0500

    Cleaned up version of 201810.jl

--- 2018/10/201810.jl
+++ 2018/10/201810.jl
@@ -20,38 +20,8 @@ function counts(vs::Vector{Int})::Dict{Int,Int}
     d
 end
 
-function metric(pos::Array{Int,2})::Int
-    THRESH = 4
-    score = 0
-    ps = sortslices(pos; dims=1)
-    i = 1
-    lstx = -1
-    lsty = -1
-    rl = 1
-    while i <= size(ps)[1]
-        x,y = ps[i, 1], ps[i, 2]
-        if x == lstx
-            if y == lsty+1
-                rl += 1
-            else
-                rl = 1
-            end
-            lsty = y
-        else
-            lstx = x
-            lsty = -1
-            if rl > THRESH
-                score += 1
-            end
-        end
-        i += 1
-    end
-    #println("returning ", score)
-    score
-end
-
 # minimum spread
-function metric2(pos::Array{Int,2})::Int
+function metric(pos::Array{Int,2})::Int
     (mnx,mxx) = extrema(pos[:,1])
     (mny,mxy) = extrema(pos[:,2])
     -(mxx-mnx) * (mxy-mny)
@@ -61,14 +31,14 @@ function sim(pos::Array{Int,2}, vel::Array{Int,2}, n::Int)::Int
     lastmt = typemin(Int)
     for i = 1:n
         pos .+= vel
-        mt = metric2(pos)
-        println(i, " ", mt)
-        #if mt <= lastmt
-        if i >= 10000 && i % 1 == 0
-            writedlm("snap/snap.$i.csv", pos, ",")
+        mt = metric(pos)
+        if mt <= lastmt
+            pos .-= vel
+            j = i - 1
+            writedlm("snap.$j.csv", pos, ",")
             println(i, " ", mt)
+            break
         end
-        i == 10400 && break
         lastmt = mt
     end
     lastmt
