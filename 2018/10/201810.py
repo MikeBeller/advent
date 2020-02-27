c9281322b3eec02533ece5f6736b3819e7db1baa
Mike Beller <bellermike@gmail.com>
2020-02-26 21:25:32 -0500

    Add python version of 201810

--- 2018/10/201810.py
+++ 2018/10/201810.py
@@ -12,7 +12,29 @@ def read_data(inpath):
             vel.append([ms[2], ms[3]])
     return (np.array(pos), np.array(vel))
 
-pos,vel = read_data("tinput.txt")
-print(pos)
-print(vel)
+def metric(pos):
+    mnx = np.amin(pos[:,0])
+    mxx = np.amax(pos[:,0])
+    mny = np.amin(pos[:,1])
+    mxy = np.amax(pos[:,1])
+    return -(mxx - mnx) * (mxy - mny)
+    
+def sim(pos, vel, n):
+    lastmt = -999999999999999
+    for i in range(n):
+        pos += vel
+        mt = metric(pos)
+        if mt <= lastmt:
+            pos -= vel
+            np.savetxt("snappy.{}.csv".format(i-1), pos, fmt="%d", delimiter=',')
+            break
+        lastmt = mt
+    return i-1
+
+def main():
+    pos,vel = read_data("input.txt")
+    r = sim(pos, vel, 20000)
+    print(r)
+
+main()
 
