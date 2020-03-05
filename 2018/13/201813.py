ebb53f14abc810ae4c05246163e08a0e045b9b88
Mike Beller <bellermike@gmail.com>
2020-03-05 17:58:22 +0200

    Use Point

--- 2018/13/201813.py
+++ 2018/13/201813.py
@@ -1,12 +1,18 @@
 from typing import List
 from dataclasses import dataclass
 
+DIRS = b'^>v<'
+
+@dataclass
+class Point:
+    x: int
+    y: int
+
 @dataclass
 class Cart:
     dr: int
-    x: int
-    y: int
-    state: int = 0
+    loc: Point
+    state: int = -1
 
 def read_data(instr: str) -> List[bytearray]:
     return [ bytearray(line.rstrip(), 'utf-8')
@@ -16,14 +22,18 @@ def locate_carts(track: List[bytearray]) -> List[Cart]:
     carts: List[Cart] = []
     for y,row in enumerate(track):
         for x,c in enumerate(row):
-            if c in bytes(b"^>v<"):
-                carts.append(Cart(c, x, y))
+            dr = DIRS.find(c)
+            if dr >= 0:
+                carts.append(Cart(dr, Point(x, y)))
     return carts
+
+def run(instr: str) -> Point:
+    pass
 
 ins = open("tinput.txt").read()
 d = read_data(ins)
 print(d)
 cs = locate_carts(d)
 print(cs)
-assert cs == [Cart(dr=ord('>'), x=2, y=0, state=0), Cart(dr=ord('v'), x=9, y=3, state=0)]
+assert cs == [Cart(dr=1, loc=Point(2, 0), state=-1), Cart(dr=2, loc=Point(9, 3), state=-1)]
 
