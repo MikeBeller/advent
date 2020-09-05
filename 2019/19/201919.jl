ba37b393d6a31b0034d49c14bb278bcb12c52bbc
Mike Beller <bellermike@gmail.com>
2020-09-05 18:12:24 -0400

    Working version of 2019-19 part 2 in Julia

--- 2019/19/201919.jl
+++ 2019/19/201919.jl
@@ -1,16 +1,23 @@
 
 include("intcode.jl")
 
-function check_coord(prog, x::Int, y::Int)
-    ic = Intcode(prog, [x, y])
-    run(ic)
-    ic.out[1]
+function check_coord(prog, x::Int, y::Int)::Int
+    if x < 0 || y < 0
+        0
+    else
+        ic = Intcode(prog, [x, y])
+        run(ic)
+        ic.out[1]
+    end
+end
+
+function read_data()
+    progstr = strip(read("input.txt", String))
+    [parse(Int, s) for s in split(progstr,",")]
 end
 
 function part_one()
-    progstr = strip(read("input.txt", String))
-    print(progstr)
-    prog = [parse(Int, s) for s in split(progstr,",")]
+    prog = read_data()
 
     println()
     s = 0
@@ -29,5 +36,43 @@ function part_one()
     println("PART ONE: ", s)
 end
 
+function find_xmin(prog::Vector{Int}, y::Int, xstart::Int, maxmove::Int)::Int
+    for x in xstart:(xstart+maxmove)
+        if check_coord(prog, x, y) == 1
+            return x
+        end
+    end
+    -1
+end
+
+function part_two()
+    prog = read_data()
+    y = 0
+    x = 0
+    found = false
+    while true
+        y += 1
+        new_xmin = find_xmin(prog, y, x, 10)
+        if new_xmin < 0
+            continue
+        end
+        x = new_xmin
+        if check_coord(prog, x+99, y-99) == 1 &&
+           check_coord(prog, x, y-99) == 1 &&
+           check_coord(prog, x+99, y) == 1
+           # check all boxes?
+           found = true
+           break
+       end
+    end
+    if found
+        println(x, " ", y-99)
+        println("PART TWO: ", x * 10000 + y - 99)
+    else
+        error("not found")
+    end
+end
+
 part_one()
+part_two()
 
