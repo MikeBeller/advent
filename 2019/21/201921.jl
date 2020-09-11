67895716f6f914e11bc2cba7f05cb6e1c2cd005f
Mike Beller <bellermike@gmail.com>
2020-09-11 18:30:00 -0400

    Possibly misguided attempt to generate all possible functions

--- 2019/21/201921.jl
+++ 2019/21/201921.jl
@@ -1,15 +1,11 @@
 
 include("intcode.jl")
 
-function run_springscript(prog, script::String)::String
+function run_springscript(prog, script::String)::Vector{Int}
     inp = [Int(c) for c in script]
     ic = Intcode(prog, inp)
     run(ic)
-    if ic.out[end] > 127
-        string(ic.out[end]) * "\n"
-    else
-        string([Char(c) for c in ic.out]...)
-    end
+    ic.out
 end
 
 function read_data()
@@ -17,15 +13,73 @@ function read_data()
     [parse(Int, s) for s in split(progstr,",")]
 end
 
-function run(sc::String)
-    prog = read_data()
+function run_print(prog::Vector{Int}, sc::String)
     out = run_springscript(prog, sc)
-    print(out)
+    if out[end] > 127
+        println(string(out[end]))
+    else
+        print(string([Char(c) for c in out]...))
+    end
 end
 
-#run("NOT D J\nWALK\n")
+prog = read_data()
+#run_print(prog, "NOT D J\nWALK\n")
 print("PART 1: ")
-run("NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n")
+run_print(prog, "NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n")
 
+function run_for_result(prog::Vector{Int}, sc::String)::Int
+    out = run_springscript(prog, sc)
+    if out[end] > 127
+        out[end]
+    else
+        -1
+    end
+end
 
-#run("NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nRUN\n")
+@assert run_for_result(prog, "NOT A J\nNOT B T\nOR T J\nNOT C T\nOR T J\nAND D J\nWALK\n") == 19360724
+@assert run_for_result(prog, "NOT D J\nWALK\n") == -1
+
+function gen_springscript(n::Int, numl::Int, cmd::String)::String
+    insts = []
+    letters = "ABCDEFGHI"
+    for i in 1:numl
+        letter = letters[i]
+        b = n & 1
+        n = n >> 1
+        if i == 1
+            push!(insts, "NOT A J")
+            if b == 1
+                push!(insts, "NOT J J")
+            end
+        else
+            if b == 0
+                push!(insts, "NOT $letter T")
+                push!(insts, "AND T J")
+            else
+                push!(insts, "AND $letter J")
+            end
+        end
+    end
+    push!(insts, cmd)
+    push!(insts, "")
+    join(insts, "\n")
+end
+
+function part_two()
+    prog = read_data()
+    res = -1
+    #for i in 0:(2^9-1)
+    for i in 0:(2^4-1)
+        sc = gen_springscript(i, 4, "WALK")
+        print(sc)
+        res = run_for_result(prog, sc)
+        if res != -1
+            break
+        end
+    end
+    res
+end
+
+r = part_two()
+println("PART 2: ", r)
+
