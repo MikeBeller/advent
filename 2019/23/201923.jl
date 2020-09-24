47be8ea5a4ec3998c241d2b946217e1dffe986d3
Mike Beller <bellermike@gmail.com>
2020-09-24 13:12:18 -0400

    Working 2019-23 part 1

--- 2019/23/201923.jl
+++ 2019/23/201923.jl
@@ -6,6 +6,14 @@ function read_data()
     [parse(Int, s) for s in split(progstr,",")]
 end
 
+function run_to_read(nic)
+    while step(nic) != 3
+        if nic.state == 99
+            error("unexpected intcode 99 termination")
+        end
+    end
+end
+
 function part_one()
     prog = read_data()
     nics = Vector{Intcode}()
@@ -14,33 +22,28 @@ function part_one()
         @assert run_to_event(nic) == 3
         push!(nic.inp, i)  # give it its network address
         run_to_event(nic)
+        push!(nics, nic)
     end
 
     while true
         for (i,nic) in enumerate(nics)
-            # NIC must always be in awaiting input
-            @assert nic.awaiting_input
-
-            # consume all waiting messages
-            if length(nic.inp > 0)
-                @assert length(nic.inp) >= 3
-                @assert run_to_event(nic) == 3
-                @assert run_to_event(nic) == 3
-                @assert run_to_event(nic) == 3
-            else
-                push!(nic.inp, -1)
-                run_to_event(nic)
+            while length(nic.inp) > 0
+                run_to_read(nic)
             end
-            while length(nic.out >= 3)
-                d = popleft!(nic.out)
-                @assert d >= 0 && d <= 49
-                x = popleft!(nic.out)
-                y = popleft!(nic.out)
+            push!(nic.inp, -1)
+            run_to_read(nic)
+
+            while length(nic.out) >= 3
+                println("$(i-1) sending to: $(nic.out[1]) x: $(nic.out[2]) y: $(nic.out[3])")
+                d = popfirst!(nic.out)
+                @assert d >= 0 && d <= 49 || d == 255
+                x = popfirst!(nic.out)
+                y = popfirst!(nic.out)
                 if d == 255
                     return y
                 end
-                push!(nics[d].inp, x)
-                push!(nics[d].inp, y)
+                push!(nics[d+1].inp, x)
+                push!(nics[d+1].inp, y)
             end
         end
     end
@@ -54,19 +57,21 @@ function test1()
     prog = read_data()
     println("PROGSIZE $(length(prog))")
     nic = Intcode(prog)
-    push!(nic.inp, 49)
+    push!(nic.inp, 0)
     println(step(nic), nic.inp, nic.out)
     while true
-        println(step(nic), nic.inp, nic.out)
+        println("STATE: $(nic.state) $(nic.inp) $(nic.out)")
         if nic.state == 99
             break
         elseif nic.state == 3
+            println("FEEDING")
             push!(nic.inp, -1)
         end
+        run_to_read(nic)
     end
 end
 
-test1()
+#test1()
 
 
 
