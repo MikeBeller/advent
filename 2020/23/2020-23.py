dac73d8b7e9ac646a5aabb123042b73df1fb3239
Mike Beller <bellermike@gmail.com>
2020-12-28 17:59:01 -0500

    Working 2020-23 in python

--- 2020/23/2020-23.py
+++ 2020/23/2020-23.py
@@ -1,50 +1,88 @@
+class Node:
+    def __init__(self, lab, nxt=None):
+        self.label = lab
+        self.nxt = nxt
 
-def mod(x, m):
-    r = x % m
-    while r < 0:
-        r += m
+def list_items(ls):
+    r = []
+    if ls == None:
+        return r
+    p = ls
+    while p is not None:
+        r.append(p.label)
+        p = p.nxt
+        if p == ls:
+            break
     return r
 
-def move(cs, c):
-    print("MOVE:", cs, c)
-    l = len(cs)
+def move(nodes, current):
+    # pick up 3 items clockwise of current
+    move_node_list = current.nxt
+    current.nxt = move_node_list.nxt.nxt.nxt
+    move_node_list.nxt.nxt.nxt = None
+    move_set = set(list_items(move_node_list))
 
-    # select 3 cups
-    mcs = cs[(c+1) % l], cs[(c+2) % l], cs[(c+3) % l]
-    print("Picking up:", mcs)
-
-    # destination cup:
-    # find cup with label equal to "current cup" - 1
-    # (if it's a selected cup keep subtracting one and
-    # also wrap around from 1 to 9)
-    dc = cs[c] - 1
-    if dc < 1: dc = 9
-    while dc in mcs:
+    # find the next label down (wrapping 1->9) which
+    # is not in the set of nodes being moved
+    dc = current.label - 1
+    if dc < 1: dc = max(nd.label for nd in nodes.values())
+    while dc in move_set:
         dc -= 1
-        if dc < 1: dc = 9
-    di = cs.index(dc)
-    print(f"Dest cup is label {dc} index {di}")
+        if dc < 1: dc = max(nd.label for nd in nodes.values())
 
     # place the cups clockwise of the "destination cup"
-    # (don't forget you have 3 "holes" in the list
-    # which you have to collapse)
-    mv = mod(di - c - 4 + 1, l)
-    print(f"need to move all left 3 from {c+4} to {di}")
-    for i in range(mv):
-        cs[(c+1 + i)%l] = cs[(c+4 +i)%l] 
-    cs[mod(di-3+1,l)],cs[mod(di-3+2,l)],cs[mod(di-3+3,l)] = mcs
-    print("New order is:", cs, "current:", (c+1)%l)
-    print()
+    dest_node = nodes[dc]
+    el = dest_node.nxt
+    dest_node.nxt = move_node_list
+    move_node_list.nxt.nxt.nxt = el
 
-    return cs, (c+1)%l
+    # new current is one node clockwise of the current
+    return current.nxt
 
+def part1(cs, nmoves):
+    nodes = {}
+    ls = p = Node(cs[0], None)
+    nodes[cs[0]] = p
+    for i in range(1,len(cs)):
+        p.nxt = Node(cs[i], None)
+        nodes[cs[i]] = p.nxt
+        p = p.nxt
+    p.nxt = ls  # circular
 
-move([3, 8, 9, 1, 2, 5, 4, 6, 7], 0)
+    current = ls
+    for i in range(nmoves):
+        current = move(nodes, current)
 
-def part1(cs, n):
-    c = 0
-    for i in range(n):
-        cs,c = move(cs, c)
-    return "".join(str(x) for x in cs)
+    return list_items(nodes[1].nxt)[:-1]
 
-print(part1([3, 8, 9, 1, 2, 5, 4, 6, 7], 10))
+assert part1([3, 8, 9, 1, 2, 5, 4, 6, 7], 10) == [9, 2, 6, 5, 8, 3, 7, 4]
+assert part1([3, 8, 9, 1, 2, 5, 4, 6, 7], 100) == [6, 7, 3, 8, 4, 5, 2, 9]
+
+print("PART1:", part1([2, 1, 9, 7, 4, 8, 3, 6, 5], 100))
+
+def part2(cs, nmoves):
+    nodes = {}
+    ls = p = Node(cs[0], None)
+    nodes[cs[0]] = p
+    for i in range(1,len(cs)):
+        p.nxt = Node(cs[i], None)
+        nodes[cs[i]] = p.nxt
+        p = p.nxt
+    for i in range(10, 1000001):
+        p.nxt = Node(i, None)
+        nodes[i] = p.nxt
+        p = p.nxt
+    p.nxt = ls  # circular
+
+    its = list_items(ls) 
+    current = ls
+    for i in range(nmoves):
+        current = move(nodes, current)
+
+    l1 = nodes[1].nxt.label
+    l2 = nodes[1].nxt.nxt.label
+
+    return l1 * l2
+
+assert part2([3, 8, 9, 1, 2, 5, 4, 6, 7], 10000000) == 149245887792
+print("PART2:", part2([2, 1, 9, 7, 4, 8, 3, 6, 5], 10000000))
