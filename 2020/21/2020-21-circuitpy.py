863aeb3a164814fb6b1983af8a15e3627f2fea3a
Mike Beller <bellermike@gmail.com>
2021-01-07 11:33:03 -0500

    Modified Janet version with new "set" module.  Also make a python version which does not work for circuit python (so it's shorter)

--- 2020/21/2020-21-circuitpy.py
+++ 2020/21/2020-21-circuitpy.py
@@ -5,6 +5,21 @@ trh fvjkl sbzzf mxmxvkd (contains dairy)
 sqjhc fvjkl (contains soy)
 sqjhc mxmxvkd sbzzf (contains fish)"""
 
+# need to write union/intersection for circuitpython
+# where set.union and set.intersection only take 2 arguments
+# otherwise these can be written set.union(*[some sequence])
+def union(xs):
+    r = set()
+    for x in xs:
+        r = r.union(x)
+    return r
+
+def intersection(xs):
+    r = set()
+    for i,x in enumerate(xs):
+        r = x if i == 0 else r.intersection(x)
+    return r
+
 def read_data(infile):
     rs = []
     for line in infile:
@@ -15,15 +30,15 @@ def read_data(infile):
     return rs
 
 def possible_assignments(rules):
-    all_algs = set.union(*[algs for (ings,algs) in rules])
+    all_algs = union(algs for (ings,algs) in rules)
     can_be = {}
     for alg in all_algs:
-        can_be[alg] = set.intersection(*[ings for (ings,algs) in rules if alg in algs])
+        can_be[alg] = intersection(ings for (ings,algs) in rules if alg in algs)
     return can_be
 
 def part1(rules):
     can_be = possible_assignments(rules)
-    all_used_ings = set.union(*can_be.values())
+    all_used_ings = union(can_be.values())
     sm = 0
     for (ings,algs) in rules:
         sm += sum(1 for i in ings if i not in all_used_ings)
