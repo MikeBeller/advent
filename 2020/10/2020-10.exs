23566829bed549d797a32788b0add9421627caa4
Mike Beller <bellermike@gmail.com>
2021-03-23 08:32:24 -0400

    Working 2020-10 in elixir

--- 2020/10/2020-10.exs
+++ 2020/10/2020-10.exs
@@ -1,7 +1,8 @@
 ExUnit.start
 defmodule Advent10.Test do
   use ExUnit.Case
-  def test_data1, do: "16 10 15 5 1 11 7 19 6 12 4"
+  def test_data, do: "16 10 15 5 1 11 7 19 6 12 4"
+  def test_data2, do: "28 33 18 42 31 14 46 20 48 47 24 23 49 45 19 38 39 11 1 32 25 35 8 17 7 9 4 2 34 10 3"
 
   def read_data(inp) do
     String.trim(inp)
@@ -16,15 +17,31 @@ defmodule Advent10.Test do
          |> Enum.frequencies()
     cs[1] * (cs[3] + 1)
   end
+
+  def part2(data) do
+    xs = [0 | data] |> Enum.sort() |> Enum.reverse()
+    mx = hd xs
+    {_n,s} = Enum.reduce(xs, [{mx+3, 1}],
+      fn x,ls -> 
+        #IO.puts "trying #{x} #{inspect ls}"
+        sm = ls
+        |> Enum.take_while(fn {n,_s} -> n - x <= 3 end)
+        |> Enum.reduce(0, fn {_n,s},acc -> acc + s end)
+        [{x, sm} | ls]
+      end) |> hd()
+    s
+  end
     
   test "test" do
-    td = read_data(test_data1())
+    td = read_data(test_data())
+    td2 = read_data(test_data2())
     assert part1(td) == 35
+    assert part1(td2) == 220
     data = read_data(File.read!("input.txt"))
-    #{us,r} = :timer.tc(fn -> part1(data, 5) end, [])
     IO.puts part1(data)
 
-    #{us,r2} = :timer.tc(Advent10.Test, :part2, [data, r])
-    #IO.puts "PART2: #{r2} TIME: #{us} us"
+    assert part2(td) == 8
+    assert part2(td2) == 19208
+    IO.puts "PART2: #{part2(data)}"
   end
 end
