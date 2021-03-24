ba9320517e0b16718641fc340692f8fedca55b99
Mike Beller <bellermike@gmail.com>
2021-03-24 10:43:31 -0400

    Part1 of 2020-11 in elixir working

--- 2020/11/2020-11.exs
+++ 2020/11/2020-11.exs
@@ -17,24 +17,58 @@ defmodule Advent11.Test do
   end
 
   def read_data(inp) do
-    String.trim(inp)
-    |> String.split("\n")
-    |> Enum.map(&to_charlist(&1))
-    |> Enum.with_index()
+    for {line,r} <- String.split(inp) |> Enum.with_index(),
+      {ch, c} <- String.to_charlist(line) |> Enum.with_index(),
+      into: %{}, do: {{r,c}, ch}
+  end
+
+  def map_to_string(m) do
+    for r <- 0..9 do
+      (for c <- 0..9, into: [], do: m[{r,c}]) |> to_string()
+    end |> Enum.join("\n")
+  end
+
+  def num_neighbors(m, r, c) do
+    (for dr <- -1..1,
+      dc <- -1..1,
+      !(dr == 0 && dc == 0), do:
+      Map.get(m, {r+dr,c+dc}, ?L)) |> Enum.count(&(&1 == ?#))
+  end
+
+  def one_gen({_, m}) do
+    m2 = for {{r,c},v} <- m, into: %{} do
+      nn = num_neighbors(m, r, c)
+      v2 = cond do
+        (v == ?L and nn == 0) -> ?#
+        (v == ?# and nn >= 4) -> ?L
+        true -> v
+      end
+      {{r,c},v2}
+    end
+    nc = Enum.count(m2, fn {k,v} -> v != m[k] end)
+    #IO.puts "RETURNING #{nc}"
+    #IO.puts map_to_string(m)
+    #IO.puts ""
+    #IO.puts map_to_string(m2)
+    {nc, m2}
   end
 
   def part1(data) do
+    {_nc, m} = {1, data}
+                |> Stream.iterate(fn {nc,m} -> one_gen({nc,m}) end)
+                |> Enum.find(fn {nc,_nf} -> nc == 0 end)
+                #|> Enum.take(3) |> hd()
+    Map.values(m) |> Enum.count(fn c -> c == ?# end)
   end
 
-  def part2(data) do
+  def part2(_data) do
   end
-    
+
   test "test" do
     td = read_data(test_data())
-    IO.inspect td
-    #assert part1(td) == 35
-    #data = read_data(File.read!("input.txt"))
-    #IO.puts "PART1: #{part1(data)}"
+    assert part1(td) == 37
+    data = read_data(File.read!("input.txt"))
+    IO.puts "PART1: #{part1(data)}"
 
     #assert part2(td) == 8
     #assert part2(td2) == 19208
