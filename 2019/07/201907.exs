bffc293c5a09ea5a8f5d30de4b24548447216b4b
Mike Beller <bellermike@gmail.com>
2021-04-04 10:15:25 -0400

    Working 201907 part 1 and 2 in elixir

--- 2019/07/201907.exs
+++ 2019/07/201907.exs
@@ -1,6 +1,6 @@
 ExUnit.start
 defmodule Advent07.Test do
-  use ExUnit.Case
+  use ExUnit.Case, async: false
 
   def read_data(inp) do
     inp
@@ -53,10 +53,10 @@ defmodule Advent07.Test do
     end
   end
 
-  def rwi(state, in_f, out_f) do
+  def run_to_end(state, in_f, out_f) do
     case step(state, in_f, out_f) do
       {99, next_state} -> next_state
-      {_op, next_state} -> rwi(next_state, in_f, out_f)
+      {_op, next_state} -> run_to_end(next_state, in_f, out_f)
     end
   end
 
@@ -68,7 +68,7 @@ defmodule Advent07.Test do
     in_f = fn [h | t] -> {h, t} end
     out_f = fn ls, v -> [v | ls] end
     state = %{m: mem, pc: 0, i: inp, o: []}
-    final_state = rwi(state, in_f, out_f)
+    final_state = run_to_end(state, in_f, out_f)
     final_state.o
   end
 
@@ -91,6 +91,42 @@ defmodule Advent07.Test do
     end)
   end
 
+  def listen(pid, last_val \\ 0, down_count \\ 0) do
+    receive do
+      {:DOWN,_,_,_p,_} -> 
+        #IO.puts "DOWN: #{inspect p}"
+        if down_count == 4 do
+          last_val
+        else
+          listen(pid, last_val, down_count+1)
+        end
+      m -> 
+        send pid, m
+        listen(pid, m, down_count)
+    end
+  end
+
+  def run_loop_phases(prog, phases) do
+    mem = list_to_mem(prog)
+    in_f = fn is -> receive do
+      v ->
+        #IO.puts "PID #{inspect self()} received #{v}"
+        {v, is}
+      end
+    end
+    out_f = fn os,v -> send(os, v); os end
+    first_pid = for ph <- Enum.reverse(phases), reduce: self() do
+      pid ->
+      ({np,_ref} = spawn_monitor(fn ->
+        run_to_end(%{m: mem, pc: 0, i: nil, o: pid}, in_f, out_f) end)
+        send np, ph
+        np)
+    end
+    send first_pid, 0
+
+    listen(first_pid)
+  end
+
   def perms([]), do: [[]]
   def perms(l) do
     for h <- l, t <- perms(l -- [h]), do: [h|t]
@@ -102,6 +138,11 @@ defmodule Advent07.Test do
     |> Enum.max()
   end
 
+  def part2(prog) do
+    perms([5, 6, 7, 8, 9])
+    |> Enum.map(fn phases -> {run_loop_phases(prog, phases), phases} end)
+    |> Enum.max()
+  end
 
   test "test intcode" do
       assert [1] == run_with_input([3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], [8])
@@ -116,12 +157,20 @@ defmodule Advent07.Test do
       assert [1001] == run_with_input(big_prog, [9393])
   end
 
-  test "run_phases" do
+  test "part1 and part2" do
+    # test part1
     assert {43210,[4, 3, 2, 1, 0]} == part1([3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0])
     assert {54321, [0, 1, 2, 3, 4]}  == part1([3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0])
     assert {65210, [1,0,4,3,2]}  == part1([3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33, 1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0])
 
     data = read_data(File.read!("input.txt"))
     IO.puts "PART1: #{inspect part1(data)}"
+
+    # test part2
+    assert 139629729 == run_loop_phases([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5], [9,8,7,6,5])
+    assert {139629729, [9, 8, 7, 6, 5]} == part2([3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5])
+    assert {18216, [9, 7, 8, 5, 6]} == part2([3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54, -5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4, 53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10])
+
+    IO.puts "PART2: #{inspect part2(data)}"
   end
 end
