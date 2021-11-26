0472043a448f50e853885f945ee90de527d16bfb
Mike Beller <mike@beller.tech>
2021-11-26 11:02:21 -0500

    Working 2021-14 part 2

--- 2018/14/201814.py
+++ 2018/14/201814.py
@@ -9,18 +9,22 @@ class State:
         self.scores: List[int] = _scores
         self.elves = [0, 1]
     
-    def step(self):
+    def step(self) -> int:
+        r = 1
+        ln = len(self.scores)
         s = sum(self.scores[e] for e in self.elves)
         #self.scores.extend(digits(s)) # 33 % slower
         if s > 9: # max of s is 19
             self.scores.append(1)
             s -= 10
+            r = 2
         self.scores.append(s)
         self.elves = [
             (e + 1 + self.scores[e]) % len(self.scores)
             for e in self.elves
         ]
-
+        return r
+                
 def scores_after_n(scores: List[int], n: int) -> List[int]:
     st = State(scores)
     while len(st.scores) < n + 10:
@@ -38,20 +42,19 @@ assert part1(2018) == "5941429882"
 
 print("PART1:", part1(330121))
 
-def part2(inp):
+def part2(inp) -> int:
     st = State([3,7])
     ln = len(inp)
     while True:
+        n = st.step()
+        if st.scores[-ln-1:-1] == inp:
+            return len(st.scores) - ln - 1
         if st.scores[-ln:] == inp:
             return len(st.scores) - ln
-        if st.scores[-ln-1:-1] == inp:
-            return len(st.scores) -ln - 1
-        st.step()
 
 assert part2([5,1,5,8,9]) == 9
 assert part2([0,1,2,4,5]) == 5
 assert part2([9,2,5,1,0]) == 18
 assert part2([5,9,4,1,4]) == 2018
 
-print(part2(digits(330121)))
-    
+print("PART2:", part2(digits(330121)))
