c4e42f6b3f2a6b0cea67be3fcdee1db61e1c39d9
Mike Beller <mike@beller.tech>
2021-12-02 10:35:22 -0500

    202102 complete

---
+++ 2021/02/202102.py
@@ -0,0 +1,41 @@
+tds = """forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2"""
+
+def parse(cmds):
+    return [(c,int(v)) for c,v in
+                [s.split() for s in cmds.splitlines()]]
+
+def part1(cmds):
+    hp,d = 0,0
+    for c,v in cmds:
+        if c == 'forward':
+            hp += v
+        elif c == 'down':
+            d += v
+        elif c == 'up':
+            d -= v
+    return hp * d
+
+td = parse(tds)
+data = parse(open("input.txt").read())
+assert part1(td) == 150
+print("PART1:", part1(data))
+
+def part2(cmds):
+    hp,d,aim = 0,0,0
+    for c,v in cmds:
+        if c == 'forward':
+            hp += v
+            d += aim * v
+        elif c == 'down':
+            aim += v
+        elif c == 'up':
+            aim -= v
+    return hp * d
+
+assert part2(td) == 900
+print("PART2:", part2(data))
