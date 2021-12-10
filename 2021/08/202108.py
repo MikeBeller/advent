c1ffd3381e59d0d3a6c0c89095318b318414e64e
Mike Beller <mike@beller.tech>
2021-12-10 18:15:30 -0500

    working 202108 -- threw out numpy too complicated

--- 2021/08/202108.py
+++ 2021/08/202108.py
@@ -1,11 +1,16 @@
 import numpy as np
 
 
+def ssort(s):
+    return "".join(sorted(s))
+
+
 def parse(instr):
     rows = []
     for line in instr.splitlines():
         s1, s2 = line.split(" | ", 1)
-        rows.append([s1.split(), s2.split()])
+        rows.append([[ssort(s) for s in s1.split()], [ssort(s)
+                    for s in s2.split()]])
     return rows
 
 
@@ -24,89 +29,66 @@ data = parse(open("input.txt").read())
 
 digit_code = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf',
               'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg']
-len_c = np.array([-1, -1, 1, 7, 4, -1, -1, 8], dtype=np.int8)
-
-
-def encode(s):
-    ix = np.frombuffer(bytes(s, encoding='utf8'), dtype=np.int8) - 97
-    r = np.zeros(7, dtype=np.int8)
-    r[ix] = 1
-    return r
-
-
-def encode_case(row):
-    ins, outs = row
-    return np.vstack([encode(i) for i in ins]), np.vstack([encode(o) for o in outs])
+len_map = {2: 1, 3: 7, 4: 4, 7: 8}
+# how much each number overlaps with 1, 4, 7, 8 respectively
+overlap = {
+    (2, 3, 3, 6): 0,
+    (1, 2, 2, 5): 2,
+    (2, 3, 3, 5): 3,
+    (1, 3, 2, 5): 5,
+    (1, 3, 2, 6): 6,
+    (2, 4, 3, 6): 9,
+}
 
 
 def part1(data):
     c = 0
-    for row in data:
-        _, outs = encode_case(row)
-        ds = len_c[np.sum(outs, axis=1)]
-        c += np.sum(ds != -1)
+    for _, outs in data:
+        for out in outs:
+            if len(out) in len_map:
+                c += 1
     return c
 
 
 assert part1(td) == 26
 print("PART1:", part1(data))
 
-known = np.array([1, 4, 7, 8])
-unknown = np.array([0, 2, 3, 5, 6, 9])
 
-
-def overlap_matrix(codes, known_inds, unknown_inds):
-    def f(a, b):
-        return np.sum(codes[a, :] & codes[b, :])
-    f_uf = np.frompyfunc(f, 2, 1)
-    return f_uf.outer(known_inds, unknown_inds)
-
-
-# Calculate the "overlap matrix" between the known digits (1, 4, 7, 8), and the unknown ones (0, 2, 3, 5, 6, 9)
-digit_codes = np.array([encode(s) for s in digit_code])
-
-
-def find_column(matrix, col):
-    for i in range(matrix.shape[1]):
-        if np.all(matrix[:, i] == col):
-            return i
-    assert False, "not found"
-
-
-def ssort(s):
-    return "".join(sorted(s))
+def nover(s1, s2):
+    return len(set(s1) & set(s2))
 
 
 def part2(data):
     sm = 0
-    std_ovm = overlap_matrix(digit_codes, known, unknown)
-    print(std_ovm)
-    for instrs, outstrs in data:
-        print(instrs, outstrs)
-        ins, _ = encode_case([instrs, outstrs])
-        lc = len_c[ins.sum(axis=1)]
-        (known_inds,) = np.nonzero(lc != -1)
-        k_i = np.argsort(lc[lc != -1])
-        k_ii = known_inds[k_i]
-        print(lc, known_inds, k_ii)
-        (unknown_inds,) = np.nonzero(lc == -1)
-        print("UK", unknown_inds)
-        u_ii = unknown_inds
-        ovm = overlap_matrix(ins, k_ii, u_ii)
-        u_iii = [find_column(ovm, std_ovm[:, i]) for i in range(len(unknown))]
-        u_iiii = unknown[u_iii]
-        print(ovm, u_iii, u_iiii)
-        p = np.zeros(10, dtype=np.int32)
-        p[k_ii] = known
-        p[u_ii] = u_iiii
-        print(p)
-        tx = {ssort(istr): ind for istr, ind in zip(instrs, p)}
-        for ostr in outstrs:
-            sostr = ssort(ostr)
-            print(sostr, tx[sostr])
-        sm += 1
+    for digit_codes, outs in data:
+        code_to_digit = {}
+        digit_to_code = {}
 
+        # find 1,4,7,8
+        other_codes = []
+        for digit_code in digit_codes:
+            ln = len(digit_code)
+            if ln in len_map:
+                code_to_digit[digit_code] = len_map[ln]
+                digit_to_code[len_map[ln]] = digit_code
+            else:
+                other_codes.append(digit_code)
+
+        # for remaining digits, find overlap with 1,4,7,8 and look up code
+        for digit_code in other_codes:
+            key = tuple(
+                nover(digit_code, digit_to_code[i]) for i in [1, 4, 7, 8])
+            d = overlap[key]
+            code_to_digit[digit_code] = d
+            # digit_to_code[d] = digit_code #don't need it
+
+        r = 0
+        for o in outs:
+            r = r * 10 + code_to_digit[o]
+        # print(r)
+        sm += r
     return sm
 
 
-print(part2(td))
+assert part2(td) == 61229
+print("PART2:", part2(data))
