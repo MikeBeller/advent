7194f92e03a186051d112e45df5829f8ed85caa8
Mike Beller <mike@beller.tech>
2021-12-12 11:06:58 -0500

    refactor 202110

--- 2021/10/202110.py
+++ 2021/10/202110.py
@@ -14,24 +14,31 @@ data = open("input.txt").read()
 
 closer = {'(': ')', '[': ']', '{': '}', '<': '>'}
 opens = set(closer.keys())
+
+
+def parse_line(line):
+    st = []
+    for c in line:
+        if c in opens:
+            st.append(c)
+        else:
+            assert c in list(closer.values())
+            if len(st) == 0 or c != closer[st[-1]]:
+                return ('ERROR', c)
+            else:
+                st.pop()
+    return ('OK', st)
+
+
 char_score = {')': 3, ']': 57, '}': 1197, '>': 25137}
 
 
 def part1(data):
     score = 0
     for line in data.splitlines():
-        st = []
-        for c in line:
-            if c in opens:
-                st.append(c)
-            else:
-                assert c in list(closer.values())
-                if len(st) == 0 or c != closer[st[-1]]:
-                    #print(line, "bad at", c)
-                    score += char_score[c]
-                    break
-                else:
-                    st.pop()
+        status, result = parse_line(line)
+        if status == 'ERROR':
+            score += char_score[result]
     return score
 
 
@@ -44,26 +51,14 @@ close_score = {'(': 1, '[': 2, '{': 3, '<': 4}
 def part2(data):
     scores = []
     for line in data.splitlines():
-        error = False
-        st = []
-        for c in line:
-            if c in opens:
-                st.append(c)
-            else:
-                if len(st) == 0 or c != closer[st[-1]]:
-                    error = True
-                    break
-                else:
-                    st.pop()
-        if error:
-            continue
-        score = 0
-        while st:
-            c = st.pop()
-            score = score * 5 + close_score[c]
-        scores.append(score)
+        status, st = parse_line(line)
+        if status == 'OK':
+            score = 0
+            while st:
+                c = st.pop()
+                score = score * 5 + close_score[c]
+            scores.append(score)
     scores.sort()
-    print(scores)
     return scores[len(scores)//2]
 
 
