ac50e698d7a6b76bf18678c7dc5f2ccda3a679aa
Mike Beller <mike@beller.tech>
2021-12-12 09:02:13 -0500

    working 2021-10

--- 2021/10/202110.py
+++ 2021/10/202110.py
@@ -37,3 +37,35 @@ def part1(data):
 
 assert part1(td) == 26397
 print("PART1:", part1(data))
+
+close_score = {'(': 1, '[': 2, '{': 3, '<': 4}
+
+
+def part2(data):
+    scores = []
+    for line in data.splitlines():
+        error = False
+        st = []
+        for c in line:
+            if c in opens:
+                st.append(c)
+            else:
+                if len(st) == 0 or c != closer[st[-1]]:
+                    error = True
+                    break
+                else:
+                    st.pop()
+        if error:
+            continue
+        score = 0
+        while st:
+            c = st.pop()
+            score = score * 5 + close_score[c]
+        scores.append(score)
+    scores.sort()
+    print(scores)
+    return scores[len(scores)//2]
+
+
+assert part2(td) == 288957
+print("PART2:", part2(data))
