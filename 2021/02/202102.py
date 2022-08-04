5552d4a79fcd239c2a754dfc595747ab39a4daa6
Mike Beller <mike@beller.tech>
2022-08-04 10:16:58 -0400

    formatting

--- 2021/02/202102.py
+++ 2021/02/202102.py
@@ -5,13 +5,15 @@ up 3
 down 8
 forward 2"""
 
+
 def parse(cmds):
-    return [(c,int(v)) for c,v in
-                [s.split() for s in cmds.splitlines()]]
+    return [(c, int(v)) for c, v in
+            [s.split() for s in cmds.strip().splitlines()]]
+
 
 def part1(cmds):
-    hp,d = 0,0
-    for c,v in cmds:
+    hp, d = 0, 0
+    for c, v in cmds:
         if c == 'forward':
             hp += v
         elif c == 'down':
@@ -20,14 +22,16 @@ def part1(cmds):
             d -= v
     return hp * d
 
+
 td = parse(tds)
 data = parse(open("input.txt").read())
 assert part1(td) == 150
 print("PART1:", part1(data))
 
+
 def part2(cmds):
-    hp,d,aim = 0,0,0
-    for c,v in cmds:
+    hp, d, aim = 0, 0, 0
+    for c, v in cmds:
         if c == 'forward':
             hp += v
             d += aim * v
@@ -36,6 +40,7 @@ def part2(cmds):
         elif c == 'up':
             aim -= v
     return hp * d
+
 
 assert part2(td) == 900
 print("PART2:", part2(data))
