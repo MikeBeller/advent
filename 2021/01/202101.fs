dbee31bf1495a00d404864d81a03d4f1db79c048
Mike Beller <mike@beller.tech>
2022-08-15 11:59:50 -0400

    Make 2021-01 compatible with gforth

--- 2021/01/202101.fs
+++ 2021/01/202101.fs
@@ -1,39 +1,41 @@
-include  ../fth/advent2021.fth
+include  ../fth/advent2021.fs
 
-: part1 { fname flen | last total num -- }
-  -1 -> last
+: part1 -1 0 0 { fname flen last total num -- }
   fname flen foreach-file-line \ ( buf n )
-    >num -> num
+    >num to num
     last -1 <>
     if num last >
       if
-        1 +-> total
+        1 total + to total
       then
     then
-    num -> last
+    num to last
   ffl-repeat
   total
   ;
 
-T{ s" tinput.txt" part1 }T{ 7 }T
+T{ s" tinput.txt" part1 -> 7 }T
 ." PART1: " s" input.txt" part1 . cr
 
-: part2 { fname flen | last1 last2 last3 total num -- }
-  -1 -> last1 -1 -> last2 -1 -> last3
+: part2 -1 -1 -1 0 0 { fname flen last1 last2 last3 total num -- }
   fname flen foreach-file-line \ ( buf n )
-    >num -> num
+    >num to num
       last1 -1 <>
       if
         num last2 + last3 +
         last1 last2 + last3 +
         >
         if
-          1 +-> total
+          1 total + to total
         then
       then
-    last2 -> last1 last3 -> last2 num -> last3
+    last2 to last1
+    last3 to last2
+    num to last3
   ffl-repeat
   total ;
 
-T{ s" tinput.txt" part2 }T{ 5 }T
+T{ s" tinput.txt" part2 -> 5 }T
 ." PART2: " s" input.txt" part2 . cr
+
+bye
