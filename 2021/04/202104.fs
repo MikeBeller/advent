a79f0da4c46d6b6a382c1591fa147c51bf77a885
Mike Beller <mike@beller.tech>
2022-08-16 16:43:13 -0400

    2021-04 in forth input system

--- 2021/04/202104.fs
+++ 2021/04/202104.fs
@@ -1,53 +1,73 @@
 include  ../fth/advent2021.fs
 
-: skip-nondigit ( buf len -- buf2 len2 )
-    begin ( buf len )
-        dup
-        if
-            over c@ ( buf len char )
-            48 58 within ( buf len isdigit? )
-            if
-                true ( buf len true )
-            else
-                1- swap 1+ swap false ( buf+1 len-1 false )
-            then
-        else
-            true
-        then
-    until ;
+3000 constant max-len
+max-len vec data
+variable ndraws 0 ndraws !
+variable nboards 0 nboards !
+100 constant mark-offset
 
-T{ create tbuf bl c, char , c, 49 c, 57 c, char , c, 52 c, 53 c, tbuf 7 skip-nondigit -> tbuf 2 + 5 }T
+: draw ( i -- n )
+    dup 0 ndraws @ within invert abort" draw index out of bounds"
+    data vec@ ;
 
-: get-num ( buf len -- buf len num ok )
-    skip-nondigit  ( buf1 len1 )
-    dup
-    if
-        dup >r
-        0 0 2swap >number ( udL udH buf2 len2 )
-        dup r> <>
-        if
-            2swap drop true ( buf2 len2 udL ok )
-        else
-            2swap 2drop 0 0
-        then
-    else
-        0 0 ( buf1 len1 0 0 )
+\ vector index of board b, row r, column c
+: board-index { b r c -- i }
+    b 0 nboards @ within invert abort" board index out of bounds"
+    r 0 5 within invert abort" invalid row index"
+    c 0 5 within invert abort" invalid column index"
+    ndraws @
+    b 25 * +
+    r 5 * +
+    c + ;
+
+: board-get ( b r c -- n )
+    board-index data vec@
+    ;
+
+: board-set ( n b r c -- )
+    board-index data vec! ;
+
+: marked? ( b r c -- t/f )
+    board-get mark-offset >= ;
+
+: mark { b r c -- }
+    b r c marked? invert if
+        b r c board-get
+        mark-offset +
+        b r c board-set
     then ;
 
-T{ tbuf 7 get-num 2swap get-num 2swap get-num 2swap 2drop -> 19 -1 45 -1 0 0 }T
+: get-unmarked { b r c -- n }
+    b r c board-get
+    dup mark-offset >= if
+        mark-offset -
+    then ;
 
-: print-nums ( buf len )
+\ Append all numbers on a line to the 'data' vector
+: read-nums ( buf len )
     begin
         get-num ( buf len num valid? )
     while
-        .
+        data vec,
     repeat 2drop drop ;
 
-: read-nums-file ( fname flen -- )
+\ Read and store all numbers in a file into data vector
+\ Keep track of the number of 'draws' ( number of numbers on first line )
+\ And number of boards 'nboards' ( number of all other numbers divided by 25 )
+: read-data ( fname flen -- )
+    data vec>clear
     foreach-file-line
-        print-nums cr
-    repeat ;
+        read-nums
+        ffl-nlines @ 1 = if
+            data vec>len ndraws !
+        then
+    ffl-repeat 
+    data vec>len ndraws @ - 25 / nboards !
+    ;
 
-s" tinput.txt" read-nums-file
+T{ s" tinput.txt" read-data ndraws @ nboards @ data vec>len -> 27 3 102 }T
+T{ 0 1 1 board-index -> 33 }T
+T{ 0 1 1 board-get 2 4 4 board-get -> 2 7 }T
+T{ 0 1 1 mark 0 1 1 board-get 0 1 1 get-unmarked 0 1 1 marked? -> 102 2 -1 }T
 
 bye
