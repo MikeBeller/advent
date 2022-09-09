dadcf19b71f05ade7bfda3a1ca69152df9895442
Mike Beller <mike@beller.tech>
2022-09-09 19:23:36 -0400

    check len

--- 2021/03/202103.lua
+++ 2021/03/202103.lua
@@ -7,11 +7,18 @@ end
 
 function read_data(path)
     local data = {}
+    local len = nil
     for line in io.lines(path) do
+        if len == nil then
+            len = string.len(line)
+        else
+            assert(len == string.len(line))
+        end
         data[#data + 1] = tonumber(line, 2)
     end
-    return data
+    return data, len
 end
 
-data = read_data("tinput.txt")
+data,len = read_data("tinput.txt")
+print(len)
 ptable(data)
