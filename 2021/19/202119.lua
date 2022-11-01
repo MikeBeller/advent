39f09bf5117bf343eceabc1b932c674975fe1335
Mike Beller <bellermike@gmail.com>
2022-11-01 17:07:07 +0200

    ...

--- 2021/19/202119.lua
+++ 2021/19/202119.lua
@@ -1,21 +1,5 @@
 inspect = require("inspect")
 
-function parse_group(gstr)
-    local group = {}
-    for x,y,z in gstr:gmatch("(-?%d+),(-?%d+),(-?%d+)") do
-        group[#group+1] = {tonumber(x), tonumber(y), tonumber(z)}
-    end
-    return group
-end
-
-function parse_data(instr)
-    local data = {}
-    for group_str in instr:gmatch("(..-)\n\n") do
-        data[#data+1] = parse_group(group_str)
-    end
-    return data
-end
-
 function parse(fpath)
     local data = {}
     local scanner = {}
@@ -33,6 +17,70 @@ function parse(fpath)
     end
     return data
 end
+
+function comp(p1, p2)
+    return p1[1] < p2[1] and p1[2] < p2[2] and p1[3] < p2[3]
+end
+
+function diff(p1, p2)
+    return p1[1] - p2[1], p1[2] - p2[2], p1[3] - p2[3]
+end
+
+function rotate(p, rotation)
+    error("unimplemented")
+    return p2
+end
+
+function rotate_all(scanner, rotation)
+    -- must return a copy of the beacons, rotated (don't mutate)
+    rotated = {}
+    for i,beacon in scanner do
+        rotated[#rotated + 1] = rotate(beacon, rotation)
+    end
+    return rotated
+end
+
+function align(aligned_scanner, scanner, rotation)
+    local rotated_scanner = rotate_all(scanner, rotation)
+    table.sort(rotated_scanner, comp)
+    for i,beacon in rotated_scanner do
+        error("unimplemented")
+    end
+    return rotated_scanner
+end
+
+function align_scanner(aligned_scanners, scanner)
+    for aligned_scanner in aligned_scanners do
+        for rotation = 1,6 do
+            local ok, al, off = align(aligned_scanner, scanner, rotation)
+            if ok then return ok, al, off end
+        end
+    end
+    return false, nil, nil
+end
+
+function align_scanners(scanners)
+    local aligned = {scanners[1]}
+    local unaligned = {}
+    for i = 2,#scanners do
+        unaligned[#unaligned + 1] = scanners[i]
+    end
+    while #unaligned != 0 do
+        for i,scanner in ipairs(unaligned) do
+            if align_scanner(aligned, scanner) then
+                table.remove(unaligned, i)
+                aligned[#aligned + 1] = scanner
+            end
+        end
+    end
+    return aligned
+end
+
+function part1(scanners)
+    local aligned_scanners = align_scanners(scanners)
+    return num_unique_beacons(aligned_scanners)
+end
+
 
 
 
