d1ad46095197edeec26875ba440daa18bcce0906
Mike Beller <bellermike@gmail.com>
2022-11-01 17:30:26 +0200

    more lua

--- 2021/19/202119.lua
+++ 2021/19/202119.lua
@@ -18,7 +18,7 @@ function parse(fpath)
     return data
 end
 
-function comp(p1, p2)
+function point_lt(p1, p2)
     return p1[1] < p2[1] and p1[2] < p2[2] and p1[3] < p2[3]
 end
 
@@ -28,7 +28,19 @@ end
 
 function rotate(p, rotation)
     error("unimplemented")
-    return p2
+    if rotation == 1 then
+        return {p[1], p[2], p[3]}
+    elseif rotation == 2 then
+        return {p[1], p[2], p[3]}
+    elseif rotation == 3 then
+        return {p[1], p[2], p[3]}
+    elseif rotation == 4 then
+        return {p[1], p[2], p[3]}
+    elseif rotation == 5 then
+        return {p[1], p[2], p[3]}
+    elseif rotation == 6 then
+        return {p[1], p[2], p[3]}
+    end
 end
 
 function rotate_all(scanner, rotation)
@@ -42,7 +54,7 @@ end
 
 function align(aligned_scanner, scanner, rotation)
     local rotated_scanner = rotate_all(scanner, rotation)
-    table.sort(rotated_scanner, comp)
+    table.sort(rotated_scanner, point_lt)
     for i,beacon in rotated_scanner do
         error("unimplemented")
     end
@@ -65,15 +77,31 @@ function align_scanners(scanners)
     for i = 2,#scanners do
         unaligned[#unaligned + 1] = scanners[i]
     end
-    while #unaligned != 0 do
+    while #unaligned ~= 0 do
         for i,scanner in ipairs(unaligned) do
             if align_scanner(aligned, scanner) then
                 table.remove(unaligned, i)
                 aligned[#aligned + 1] = scanner
+                break
             end
         end
     end
     return aligned
+end
+
+function num_unique_beacons(scanners)
+    local count = 0
+    local tab = {}
+    for scanner in scanners do
+        for beacon in beacons do
+            local bs = table.concat(beacon, ",")
+            if not tab[bs] then
+                tab[bs] = true
+                count = count + 1
+            end
+        end
+    end
+    return count
 end
 
 function part1(scanners)
