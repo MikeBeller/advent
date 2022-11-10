864049d0a40fcd091e0981ba754fa406477d2102
Mike Beller <mike@beller.tech>
2022-11-10 17:11:26 -0500

    test works but not full run

--- 2021/20/202120.py
+++ 2021/20/202120.py
@@ -3,40 +3,37 @@ def cvt(c):
     return 1 if c == '#' else 0
 
 
-class Image:
-    def __init__(self):
-        self.data = {}
-        self.min_r = float("inf")
-        self.max_r = float("-inf")
-        self.min_c = float("inf")
-        self.max_c = float("-inf")
+def minmax(xs):
+    xs = list(xs)
+    return min(xs), max(xs)
 
-    def get(self, r, c):
-        return self.data.get((r, c), 0)
 
-    def set(self, r, c, v):
-        self.min_r = min(r, self.min_r)
-        self.max_r = max(r, self.max_r)
-        self.min_c = min(c, self.min_c)
-        self.max_c = max(c, self.max_c)
-        self.data[(r, c)] = v
+def ranges(img):
+    keys = [k for k, v in img.items() if v == 1]
+    rmn, rmx = minmax(r for (r, _) in keys)
+    cmn, cmx = minmax(c for (_, c) in keys)
+    return range(rmn - 2, rmx + 3), range(cmn - 2, cmx + 3)
 
-    def show(self):
-        for r in range(self.min_r, self.max_r + 1):
-            for c in range(self.min_c, self.max_c + 1):
-                ch = '#' if self.get(r, c) else "."
-                print(ch, sep='', end='')
-            print()
+
+def show(img):
+    rrange, crange = ranges(img)
+    for r in rrange:
+        print(f"{r:03} ", sep="", end="")
+        for c in crange:
+            ch = '#' if img.get((r, c), 0) else "."
+            print(ch, sep='', end='')
+        print()
+    print()
 
 
 def parse(instr):
     alg_s, img_s = instr.split("\n\n")
     alg = [cvt(c) for c in alg_s]
-    img = Image()
+    img = {}
     for r, line in enumerate(img_s.splitlines()):
         for c, ch in enumerate(line):
             v = cvt(ch)
-            img.set(r, c, v)
+            img[(r, c)] = v
     return alg, img
 
 
@@ -44,27 +41,28 @@ talg, timg = parse(open("tinput.txt").read())
 
 
 def enhance(alg, img):
-    img2 = Image()
-    for r in range(img.min_r, img.max_r + 1):
-        for c in range(img.min_c, img.max_c + 1):
-            b = img.get(r, c)
+    img2 = {}
+    rrange, crange = ranges(img)
+    for r in rrange:
+        for c in crange:
             n = 0
             for ri in range(-1, 2):
                 for ci in range(-1, 2):
                     n = 2 * n + img.get((r + ri, c + ci), 0)
-                    print(r, c, n)
             p = alg[n]
-            img2.set(r, c, p)
+            img2[(r, c)] = p
     return img2
 
 
 def part1(alg, img):
+    show(img)
     img2 = enhance(alg, img)
-    img2.show()
+    show(img2)
     img3 = enhance(alg, img2)
-    img3.show()
-    print(img3.__dict__)
-    return sum(img3.data.values())
+    show(img3)
+    return sum(img3.values())
 
 
-print(part1(talg, timg))
+assert part1(talg, timg) == 35
+alg, img = parse(open("input.txt").read())
+print("PART1:", part1(alg, img))
