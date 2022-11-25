0b1cd3914445ffedcfc9a1749864ec83d131a44d
Mike Beller <mike@beller.tech>
2022-11-24 23:47:04 -0500

    run

--- 2021/24/202124.lua
+++ 2021/24/202124.lua
@@ -1,5 +1,6 @@
 require 'pl'
 stringx.import()
+local unpack = table.unpack
 
 local function parg(s)
     return tonumber(s) or s
@@ -18,5 +19,33 @@ local function parse(instr)
     return List(instr:lines()):map(parse_inst)
 end
 
+local function resolve(st, op)
+    return (type(op) == "number") and op or st[op]
+end
+
+local function run(insts, inp_iter)
+    local st = { w = 0, x = 0, y = 0, z = 0 }
+    for inst in insts:iter() do
+
+        cmd, op1, op2 = unpack(inst)
+        if cmd == "inp" then
+            st[op1] = next(inp_iter)
+        elseif cmd == "add" then
+            st[op1] = st[op1] + resolve(st, op2)
+        elseif cmd == "mul" then
+            st[op1] = st[op1] * resolve(st, op2)
+        elseif cmd == "div" then
+            st[op1] = st[op1] / resolve(st, op2)
+        elseif cmd == "mod" then
+            st[op1] = st[op1] % resolve(st, op2)
+        elseif cmd == "eql" then
+            st[op1] = (st[op1] == resolve(st, op2)) and 1 or 0
+        else
+            error("invalid op", cmd)
+        end
+    end
+    return st
+end
+
 local tinput = parse(io.input("tinput.txt"):read("*a"))
-print(tinput)
+print(run(tinput, seq({ 7 })))
