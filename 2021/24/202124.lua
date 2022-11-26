b8a020427297084cc24a59f0287b807bdfa41de2
Mike Beller <mike@beller.tech>
2022-11-26 12:10:20 -0500

    trying digit by digit

--- 2021/24/202124.lua
+++ 2021/24/202124.lua
@@ -25,32 +25,29 @@ local function resolve(st, op)
     return (type(op) == "number") and op or st[op]
 end
 
-local function istream(ls)
-    return coroutine.wrap(function()
-        for _, v in ipairs(ls) do
-            coroutine.yield(v)
-        end
-    end)
-end
-
-local function model_num(mns)
-    return istream(
-        seq(List.iterate(mns)):map(tonumber):copy()
-    )
-end
-
 local function sgn(x)
     return (x < 0) and -1 or 1
 end
 
-local function run(insts, inp_stream)
+local mtab = seq(seq.range(0, 13)):map(function(x) return 10 ^ x end):copy()
+print(mtab)
+
+local function run(prog, input)
+    --print("START")
+    local iind = 14
     local st = Map { w = 0, x = 0, y = 0, z = 0 }
-    local tmp, atmp
-    for inst in insts:iter() do
+    local tmp
+    for inst in prog:iter() do
         -- print(st, inst)
         local cmd, op1, op2 = unpack(inst)
         if cmd == "inp" then
-            st[op1] = inp_stream()
+            local v = floor(input / mtab[iind]) % 10
+            print(inst, st, v)
+            if v == 0 then
+                return false, st
+            end
+            iind = iind - 1
+            st[op1] = v
         elseif cmd == "add" then
             st[op1] = st[op1] + resolve(st, op2)
         elseif cmd == "mul" then
@@ -66,29 +63,40 @@ local function run(insts, inp_stream)
             error("invalid op", cmd)
         end
     end
-    return st
+    return true, st
 end
 
 local tinput = parse(io.input("tinput.txt"):read("*a"))
-assert(run(tinput, istream({ 11 })) == Map { w = 1, x = 0, y = 1, z = 1 })
+--assert(run(tinput, istream({ 11 })) == Map { w = 1, x = 0, y = 1, z = 1 })
 
-local input = parse(io.input("input.txt"):read("*a"))
---print(run(input, model_num("13579246899999")))
---print(run(input, model_num("13579246900000")))
---print(run(input, model_num("99999999999999")))
+local prog = parse(io.input("input.txt"):read("*a"))
+--print(model_num("12345678912345"))
+--assert(select(2, run(prog, 13579246899999)).z == 25910832)
+print(run(prog, 11111111111111))
+--print(run(prog, model_num("99999999999999")))
 
-local function part1(input)
-    for i = 1111111, 9999999 do
-        local si = tostring(i)
-        if not si:find('0') then
-            --local mn = "999999999" .. si
-            local mn = si .. "999999999"
-            local st = run(input, model_num(mn))
+local function part1(prog)
+    local prefix = 0
+    local ok, st
+    for d = 0, 13 do
+        local mpy = 10 ^ (13 - d)
+        local found = false
+        for i = 1, 9 do
+            local mn = (prefix * 10 + i) * mpy
+            ok, st = run(prog, mn)
             if st.z == 0 then
-                print("FOUND", mn)
+                print("locked in:", i, st)
+                prefix = 10 * prefix + i
+                found = true
+                break
             end
         end
+        if not found then
+            print("NOT FOUND", st)
+            return false, st
+        end
     end
+    return prefix, st
 end
 
-print(part1(input))
+part1(prog)
