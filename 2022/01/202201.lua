f61bd8e87e48019ca4d9df639f3d699f614e7348
Mike Beller <mike@beller.tech>
2022-12-01 11:47:17 -0500

    working 202201

--- 2022/01/202201.lua
+++ 2022/01/202201.lua
@@ -1,22 +1,37 @@
 
-function part1()
-    local calories = {0}
-    local ei = 1
-    local mxi = 1
-    for line in io.lines("input.txt") do
-        if line == "" then
-            calories[#calories+1] = 0
-            ei = ei + 1
-        else
-            local n = tonumber(line)
-            calories[ei] = calories[ei] + n
-            if calories[ei] > calories[mxi] then
-                mxi = ei
-            end
-        end
-    end
-    return calories[mxi]
-end
+-- 'pure' lua version
+-- function part1()
+--     local calories = {0}
+--     local ei = 1
+--     local mxi = 1
+--     for line in io.lines("input.txt") do
+--         if line == "" then
+--             calories[#calories+1] = 0
+--             ei = ei + 1
+--         else
+--             local n = tonumber(line)
+--             calories[ei] = calories[ei] + n
+--             if calories[ei] > calories[mxi] then
+--                 mxi = ei
+--             end
+--         end
+--     end
+--     return calories[mxi]
+-- end
 
+require 'pl.strict'
+local seq = require 'pl.seq'
+require('pl.stringx').import()
 
-print(part1())
+local input = io.input("input.txt"):read('*a')
+
+local elves = input:split("\n\n"):map(function (group)
+    return seq.sum(group:splitlines():map(tonumber))
+end)
+
+local _, ans1 = seq.minmax(elves)
+print("PART1:", ans1)
+
+elves:sort()
+local ans2 = seq.sum(elves:slice(-3,-1))
+print("PART2:", ans2)
