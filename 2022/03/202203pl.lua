4e84b161482cb5f368395fdb6fcb6ca3fa077e09
MikeBeller <mike@beller.tech>
2022-12-04 12:42:45 +0000

    working penlight version

--- 2022/03/202203pl.lua
+++ 2022/03/202203pl.lua
@@ -1,7 +1,5 @@
 require 'pl'
 require 'pl.strict'
-stringx.import()
-local sub = string.sub
 
 local alphas = List("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 local priority = tablex.index_map(alphas)
@@ -12,9 +10,7 @@ function part1(lines)
         local ln2 = #line/2
         local a = Set(List(line):slice(1,ln2))
         local b = Set(List(line):slice(ln2+1,-1))
-        print(a, b)
-        local cm = a * b
-        print(cm:values())
+        local cm = Set.values(Set.intersection(a, b))
         score = score + priority[cm[1]]
     end
     return score
@@ -23,4 +19,18 @@ end
 local tinput = io.input("tinput.txt"):read("*a"):splitlines()
 local input = io.input("input.txt"):read("*a"):splitlines()
 
-print(part1(tinput))
+assert(part1(tinput) == 157)
+print("PART1:", part1(input))
+
+function part2(lines)
+    local score = 0
+    for i = 1,#lines-2,3 do
+        local a,b,c = Set(List(lines[i])), Set(List(lines[i+1])), Set(List(lines[i+2]))
+        local cm = Set.values(Set.intersection(Set.intersection(a, b), c))
+        score = score + priority[cm[1]]
+    end
+    return score
+end
+
+assert(part2(tinput) == 70)
+print("PART2:", part2(input))
