d50326f4c7a869272bf9487e1df3d81c07621898
MikeBeller <mike@beller.tech>
2022-12-22 21:47:33 +0000

    fixing

--- 2022/07/202207.py
+++ 2022/07/202207.py
@@ -15,10 +15,44 @@ tinput = parse_input(open('tinput.txt', 'r').read())
 
 print(tinput)
 
-def build_tree(t, cmds):
-  if cmds == []:
-    return t
-  else:
+def build_tree(cmds):
+  cmds = list(reversed(cmds[1:]))
+  tree = {}
+  path = [tree]
+  while cmds:
     cmd = cmds.pop()
-    if cmd['cmd'] == 'cd':
+    match cmd:
+      case {'cmd': 'cd', 'args': ".."}:
+        path.pop()
+      case {'cmd': 'cd', 'args': d}:
+        path.append(path[-1][d])
+      case {'cmd': 'ls', 'args': items}:
+        for item in items:
+          match item:
+            case ['dir', d]:
+              path[-1][d] = {}
+            case [sz, nm]:
+              path[-1][nm] = int(sz)
+  return tree
+
+def sizes(tree):
+  """For each directory, add a 'size' field containing
+  the total size of all files in the directory, and all files
+  in subdirectories."""
+  tree['size'] = 0
+  for _, v in tree.items():
+    if isinstance(v, int):
+      tree['size'] += v
+    else:
+      sizes(v)
+      tree['size'] += v['size']
+
+
+def part1(cmds):
+  tree = build_tree(cmds)
+  sizes(tree)
+  return tree
+
+print(part1(tinput))
+
       
