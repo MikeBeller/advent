7688129e7bd196c7f6fc8946ead687e50d5ae750
MikeBeller <mike@beller.tech>
2022-12-24 22:36:40 +0000

    coolness

--- 2022/07/202207.exs
+++ 2022/07/202207.exs
@@ -3,27 +3,56 @@ defmodule Advent07 do
     [cstr | rest] = String.split(cmd_str, "\n")
     case String.split(cstr, " ") do
       ["cd", dir] -> %{cmd: :cd, args: dir}
-      ["ls"] -> %{cmd: :ls, args: (for line <- rest, do: String.split(line, " "))}
+      ["ls"] -> %{cmd: :ls, args:
+        (for line <- rest, !String.starts_with?(line, "dir") do
+          case String.split(line)do
+            [k, v] -> [v, String.to_integer(k)]
+          end
+        end)
+      }
     end
   end
 
   def parse_input(input) do
     input
-    |> String.split("\n?$ ")
+    |> String.split("\n$ ")
     |> Enum.slice(1..-1)
     |> Enum.map(&parse_cmd/1)
   end
 
   def apply_cmd(cmd, %{path: path, nodes: nodes} = state) do
     case cmd do
-      %{cmd: :cd, args: ".."} -> %{state | path: Enum.take(path, -1)}
-      %{cmd: :cd, args: dir} -> %{state | path: path ++ [dir]}
-      %{cmd: :ls, args: args} -> IO.inspect(args); state
+      %{cmd: :cd, args: ".."} -> %{state | path: (tl path)}
+      %{cmd: :cd, args: dir} -> %{state | path: [dir | path]}
+      %{cmd: :ls, args: args} ->
+        nodes =
+          args
+          |> Enum.reduce(nodes, fn [name, size], nodes ->
+            Map.put(nodes, [name | path], size)
+          end)
+
+        %{state | nodes: nodes}
     end
+  end
+
+  def apply_cmds(cmds), do: cmds |> Enum.reduce(%{path: ["/"], nodes: %{}}, &apply_cmd/2)
+
+  def rollup(nodes) do
+    keys = Map.keys(nodes) |> Enum.sort_by(&length/1, :desc)
+    IO.inspect(keys)
+    keys |> Enum.reduce(%{}, fn key, acc ->
+      case key do
+        [name] -> Map.put(acc, name, nodes[key])
+        [name | rest] ->
+          Map.put(acc, name, nodes[key] + Map.get(acc, rest, 0))
+      end
+    end)
   end
 end
 
-
-tinput = Advent07.parse_input(File.read!("tinput.txt"))
-IO.inspect(tinput)
-IO.inspect(tinput |> Enum.reduce(%{path: [], nodes: []}, &Advent07.apply_cmd/2))
+alias Advent07, as: A
+tinput = A.parse_input(File.read!("tinput.txt"))
+#IO.inspect(tinput)
+st = tinput |> A.apply_cmds()
+IO.inspect(st)
+IO.inspect(A.rollup(st.nodes))
