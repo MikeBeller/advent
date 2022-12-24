d17d0e824970416caabc504afcab22263f33f6df
MikeBeller <mike@beller.tech>
2022-12-24 21:32:27 +0000

    more

--- 2022/07/202207.exs
+++ 2022/07/202207.exs
@@ -9,11 +9,21 @@ defmodule Advent07 do
 
   def parse_input(input) do
     input
-    |> String.split("$ ")
+    |> String.split("\n?$ ")
     |> Enum.slice(1..-1)
     |> Enum.map(&parse_cmd/1)
   end
+
+  def apply_cmd(cmd, %{path: path, nodes: nodes} = state) do
+    case cmd do
+      %{cmd: :cd, args: ".."} -> %{state | path: Enum.take(path, -1)}
+      %{cmd: :cd, args: dir} -> %{state | path: path ++ [dir]}
+      %{cmd: :ls, args: args} -> IO.inspect(args); state
+    end
+  end
 end
+
 
 tinput = Advent07.parse_input(File.read!("tinput.txt"))
 IO.inspect(tinput)
+IO.inspect(tinput |> Enum.reduce(%{path: [], nodes: []}, &Advent07.apply_cmd/2))
