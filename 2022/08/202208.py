be435198cc9710df6f565b302656b4d6257ba90e
miketest3k <bellermike@gmail.com>
2022-12-27 23:27:29 +0000

    working on 08 in python

--- 2022/08/202208.py
+++ 2022/08/202208.py
@@ -1,36 +1,49 @@
 
 def parse(instr):
-    return [
-        [int(c) for c in line]
-        for line in instr.splitlines()
-    ]
+  g = {(r,c): int(ch)
+            for r,line in enumerate(instr.splitlines())
+            for c,ch in enumerate(line)
+      }
+  nr = max(r for r,c in g.keys()) + 1
+  nc = max(c for r,c in g.keys()) + 1
+  g['nr'], g['nc'] = nr, nc
+  return g
+
+def viz(g, v, nr, nc):
+  for r in range(nr):
+    for c in range(nc):
+      ch = g[r,c] if v.get((r,c)) else '.'
+      print(ch, end='')
+    print()
+  print()
 
 def part1(g):
-    nr, nc = len(g), len(g[0])
+    nr, nc = g['nr'], g['nc']
     visible = {}
-    for r in range(1,nr-1):
-        for c in range(1,nc):
-            if g[r][c] > g[r][c-1]:
+    for r in range(nr):
+        for c in range(nc):
+            if g[r,c] > g.get((r,c-1),-1):
                 visible[r,c] = True
             else:
                 break
-        for c in range(nc-2,0,-1):
-            if g[r][c] > g[r][c+1]:
+        for c in range(nc-1,-1,-1):
+            if g[r,c] > g.get((r, c+1), -1):
                 visible[r,c] = True
             else:
                 break
-    for r in range(nr-1,0,-1):
-        for c in range(1,nc):
-            if g[r][c] > g[r][c-1]:
+    for c in range(nc):
+        for r in range(nr):
+            if g[r,c] > g.get((r-1, c),-1):
                 visible[r,c] = True
             else:
                 break
-        for c in range(nc-2,0,-1):
-            if g[r][c] > g[r][c+1]:
+        for r in range(nr-1,-1,-1):
+            if g[r,c] > g.get((r+1, c), -1):
                 visible[r,c] = True
             else:
                 break
-    return sum(visible.values()) + 2 * nr + 2 * nc - 4
+    viz(g, visible, g['nr'], g['nc'])
+    return len(visible)
 
 tinput = parse(open('tinput.txt').read())
 print(part1(tinput))
