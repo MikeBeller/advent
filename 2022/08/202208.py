336356b54bfeffe6b520d65012d542732417bfdd
miketest3k <bellermike@gmail.com>
2023-03-09 16:39:48 +0000

    Number 8 in python complete

--- 2022/08/202208.py
+++ 2022/08/202208.py
@@ -32,7 +32,44 @@ def part1(gr):
     return len(vis)
 
 
+
 tinput = parse(open('tinput.txt').read())
 assert part1(tinput) == 21
 input = parse(open('input.txt').read())
 print("PART1:", part1(input))
+
+def part2(gr):
+  nr, nc = len(gr), len(gr[0])
+  return max(score(gr, r, c, nr, nc) for r in range(nr) for c in range(nc))
+
+def score(gr, r, c, nr, nc):
+  return prod(drscore(gr, r, c, nr, nc, dr) for dr in range(4))
+
+from functools import reduce
+def prod(lst):
+  return reduce(lambda x, y: x*y, lst, 1)
+
+def drscore(gr, r, c, nr, nc, dr):
+  s = 0
+  h = gr[r][c]
+  rr,cc = move(r, c, dr)
+  while rr < nr and cc < nc and rr >= 0 and cc >= 0:
+    s += 1
+    if gr[rr][cc] >= h:
+      break
+    rr, cc = move(rr, cc, dr)
+  #print(r, c, dr, s)
+  return s
+
+def move(r, c, dr):
+  if dr == 0:
+    return r-1, c
+  elif dr == 1:
+    return r, c + 1
+  elif dr == 2:
+    return r + 1, c
+  elif dr == 3:
+    return r, c-1
+
+assert score(tinput, 1, 2, len(tinput), len(tinput[0])) == 4
+print("PART2:", part2(input))
