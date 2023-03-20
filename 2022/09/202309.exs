2144c2bcca3c849e5ec9b00a2a93d873a91ca7e1
MikeBeller <mike@beller.tech>
2023-03-20 20:08:57 +0000

    nearly working 09 part 1

--- 2022/09/202309.exs
+++ 2022/09/202309.exs
@@ -1,11 +1,11 @@
 defmodule Advent09 do
   def parse_command(line) do
-    [command, value] = String.split(line, " ")
+    [command, value] = line |> String.trim() |> String.split(" ")
     {String.to_atom(command), String.to_integer(value)}
   end
 
   def read_data(fpath) do
-    input = File.stream!(fpath)
+    File.stream!(fpath)
       |> Enum.map(&parse_command/1)
   end
 
@@ -14,42 +14,24 @@ defmodule Advent09 do
   end
 
   def catch_up({hr, hc}, {tr, tc}) do
-    # if they are in the same row and more than 1 space apart, move the tail one step toward the head
-    if hr == tr and abs(hc - tc) > 1 do
-      if hc > tc do
-        {tr, tc + 1}
-      else
-        {tr, tc - 1}
-      end
-    # if they are in the same column and more than 1 space apart, move the tail one step toward the head
-    else if hc == tc and abs(hr - tr) > 1 do
-      if hr > tr do
-        {tr + 1, tc}
-      else
-        {tr - 1, tc}
-      end
-    # else if they are neither in the same row nor the same column, move the tail one step diagonally toward the head
-    else
-      if hr > tr do
-        if hc > tc do
-          {tr + 1, tc + 1}
-        else
-          {tr + 1, tc - 1}
-        end
-      else
-        if hc > tc do
-          {tr - 1, tc + 1}
-        else
-          {tr - 1, tc - 1}
-        end
-      end
-    # else don't move the tail
-    else
-      {tr, tc}
+    cond do
+      hr == tr and abs(hc - tc) == 2 ->
+        {tr, tc + div(hc - tc, 2)}
+
+      hc == tc and abs(hr - tr) == 2 ->
+        {tr + div(hr - tr, 2), tc}
+
+      hr != tr and hc != tc ->
+        dr = div(hr - tr, abs(hr - tr))
+        dc = div(hc - tc, abs(hc - tc))
+        {tr + dr, tc + dc}
+
+      true ->
+        {tr, tc}
     end
   end
 
-  def move(dir, %{h: {hr, hc}, t: {tr, tc}, visited: visited} = state) do
+  def move(dir, %{h: {hr, hc}, t: {tr, tc}, visited: visited} = _state) do
     {hr, hc} = case dir do
       :U -> {hr - 1, hc}
       :R -> {hr, hc + 1}
@@ -76,6 +58,12 @@ defmodule Advent09 do
   def process_commands(commands) do
     Enum.reduce(commands, %{h: {0, 0}, t: {0, 0}, visited: MapSet.new()}, &process_command/2)
   end
+
+  def part1(fpath) do
+    commands = read_data(fpath)
+    state = process_commands(commands)
+    MapSet.size(state.visited)
+  end
 end
 
 ExUnit.start()
@@ -91,10 +79,26 @@ defmodule AdventO9Test do
     assert parse_command("L 4") == {:L, 4}
   end
 
-  test "move" do
+  test "move touching" do
     assert move(:U, %{h: {0, 0}, t: {0, 0}, visited: MapSet.new()}) == %{h: {-1, 0}, t: {0, 0}, visited: MapSet.new([{0, 0}])}
     assert move(:R, %{h: {0, 0}, t: {0, 0}, visited: MapSet.new()}) == %{h: {0, 1}, t: {0, 0}, visited: MapSet.new([{0, 0}])}
     assert move(:D, %{h: {0, 0}, t: {0, 0}, visited: MapSet.new()}) == %{h: {1, 0}, t: {0, 0}, visited: MapSet.new([{0, 0}])}
     assert move(:L, %{h: {0, 0}, t: {0, 0}, visited: MapSet.new()}) == %{h: {0, -1}, t: {0, 0}, visited: MapSet.new([{0, 0}])}
+  end
+
+  test "move horizontal or vertical" do
+    assert move(:R, %{h: {0, 1}, t: {0, 0}, visited: MapSet.new([{0, 0}])})
+      == %{h: {0, 2}, t: {0, 1}, visited: MapSet.new([{0, 0}, {0, 1}])}
+    assert move(:U, %{h: {-1, 0}, t: {0, 0}, visited: MapSet.new([{0, 0}])})
+      == %{h: {-2, 0}, t: {-1, 0}, visited: MapSet.new([{0, 0}, {-1, 0}])}
+  end
+
+  test "move diagonal" do
+    assert move(:D, %{h: {1, 1}, t: {0, 0}, visited: MapSet.new([{0, 0}])})
+      == %{h: {2, 1}, t: {1, 1}, visited: MapSet.new([{0, 0}, {1, 1}])}
+  end
+
+  test "tinput" do
+    assert part1("tinput.txt") == 13
   end
 end
