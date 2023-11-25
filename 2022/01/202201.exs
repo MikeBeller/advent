d4e00faaa5d6fa7ee3c7a416af48c706f01e64a7
MikeBeller <bellermike@gmail.com>
2023-11-25 19:50:38 +0000

    reduce fat in 202201 elixir

--- 2022/01/202201.exs
+++ 2022/01/202201.exs
@@ -1,31 +1,20 @@
 defmodule Advent01 do
+  import Enum
   def parse(input) do
     input
     |> String.split("\n\n")
-    |> Enum.map(fn s -> s
+    |> map(fn s -> s
         |> String.split("\n")
-        |> Enum.map(&String.to_integer/1)
-        end)
+        |> map(&String.to_integer/1)
+      end)
   end
 
-  def part1(data) do
-    data
-    |> Enum.map(&Enum.sum/1)
-    |> Enum.max()
-  end
+  def part1(data), do: data |> map(&sum/1) |> max()
 
-  def part2(data) do
-    data
-    |> Enum.map(&Enum.sum/1)
-    # sort the sums in descending order, take the first 3, and add them
-    |> Enum.sort(&>=/2)
-    |> Enum.take(3)
-    |> Enum.sum()
-  end
+  def part2(data), do: data |> map(&sum/1) |> sort(&>=/2) |> take(3) |> sum()
 end
 
 input = File.read!("input.txt")
 data = Advent01.parse(input)
 IO.puts(Advent01.part1(data))
-
 IO.puts(Advent01.part2(data))
