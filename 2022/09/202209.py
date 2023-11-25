28666810b6d2c2c34f6ac0308909d27e4bd1e674
MikeBeller <bellermike@gmail.com>
2023-11-25 15:03:17 +0000

    09 complete in python

--- 2022/09/202209.py
+++ 2022/09/202209.py
@@ -30,8 +30,6 @@ assert move_tail((2, 2), (2, 1)) == (2, 1)
 assert move_tail((1, 1), (-1, 2)) == (0, 1)
 
 def dump(H, T, visited):
-  # for r in range(min(H[0], T[0])-1, max(H[0], T[0])+2):
-  #     for c in range(min(H[1], T[1])-1, max(H[1], T[1])+2):
   for r in range(-5,6):
     for c in range(-5, 6):
       if (r, c) == T:
@@ -40,13 +38,11 @@ def dump(H, T, visited):
         print('H', end='')
       elif (r, c) in visited:
         print('#', end='')
-        # print(f'{r} {c}', end='')
       else:
         print('•', end='')
     print()
   print()
 
-            
 
 def part1(data):
     visited = set([(0, 0)])
@@ -60,9 +56,26 @@ def part1(data):
           #dump(H,T,visited)
     return len(visited)
 
-
-
 tinput = parse_input(open("tinput.txt").read())
 assert part1(tinput) == 13
 input = parse_input(open("input.txt").read())
 print("PART1:", part1(input))
+
+def part2(data):
+  visited = set([(0, 0)])
+  knots = [(0,0)] * 10
+  for si,(s, t) in enumerate(data):
+      #print("STEP:", si, s, t)
+      for i in range(t):
+        knots[0] = move(knots[0], s, 1)
+        for k in range(1,10):
+          knots[k] = move_tail(knots[k-1], knots[k])
+        visited.add(knots[-1])
+        #dump(H,T,visited)
+  return len(visited)
+
+assert part2(tinput) == 1
+tinput2 = parse_input(open("tinput2.txt").read())
+assert part2(tinput2) == 36
+
+print("PART2:", part2(input))
