9572380ca5ef60ee4f140844f57c9c196f9b950c
MikeBeller <bellermike@gmail.com>
2023-11-25 19:40:42 +0000

    tweak lua version

--- 2022/01/202201_tiny.lua
+++ 2022/01/202201_tiny.lua
@@ -1,7 +1,7 @@
 require 'pl'
 stringx.import()
-data = io.input("input.txt"):read('*a'):split("\n\n"):map(
-  function (g) return seq.sum(g:splitlines():map(tonumber)) end)
+input = io.input("input.txt"):read('*a'):split("\n\n")
+data = input:map(function (g) return seq.sum(g:splitlines():map(tonumber)) end)
 a,b = seq.minmax(data)
 print(b)
 data:sort()
