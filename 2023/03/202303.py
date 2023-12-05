3b197d063e66da76c028ff66a6847e6f4dcd0ba5
MikeBeller <bellermike@gmail.com>
2023-12-05 21:38:40 +0000

    Working part 1

--- 2023/03/202303.py
+++ 2023/03/202303.py
@@ -1,38 +1,35 @@
 from advent import *
 from collections import defaultdict
-digits = "0123456789"
-tinput = lines("tinput.txt")
 
-def locs(input):
-  data = defaultdict(dict)
-  number = []
-  for r,line in enumerate(input):
-    for c,ch in enumerate(line):
-      if number and ch not in digits:
-        number = []
-      if ch == '.': continue 
-      if ch in digits:
-        data[r][c] = ('num', ch, number)
-      else:
-        data[r][c] = ('sym', ch, None)
-  return data    
-      
-def part1(input):
-  nr = len(input)
-  nc = len(input[0])
-  data = locs(input)
-  found_nums = set()
-  for r,row in data.items():
-    for c,item in data[r].items():
-      ty,ch,nm = item
-      if ty == 'sym':
-        for ri in range(r-1,r+2):
-          for ci in range(c-1, c+2):
-            if ri in data and ci in data[ri]:
-              item = data[ri][ci]
-              if item[0] == 'num':
-                print(data[ri][ci])
+tinput = lines("tinput.txt") 
+input = lines("input.txt")
 
-        
+def touching_symbol(data,r,c):
+  nr,nc = len(data),len(data[0])
+  for dr in range(max(r-1,0),min(r+2,nr)):
+    for dc in range(max(c-1,0),min(c+2,nc)):
+      ch = data[dr][dc]
+      if not ch.isnumeric() and ch != ".":
+        return True
+  return False
       
+def part1(data):
+  sm = 0
+  for r,row in enumerate(data):
+    itr = enumerate(row)
+    for c,ch in itr:
+      v = 0
+      include = False
+      while ch.isnumeric():
+        if touching_symbol(data,r, c):
+          include = True
+        v = 10 * v + int(ch)
+        if c == len(row)-1:
+          break
+        c,ch = next(itr)
+      if include:
+        print(v)
+        sm += v
+  return sm
   
+print(part1(input))
