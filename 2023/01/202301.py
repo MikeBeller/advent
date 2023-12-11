d6390e7cbca3187dfa6fbb889fead31c4a565dd2
MikeBeller <bellermike@gmail.com>
2023-12-11 02:01:02 +0000

    Redo 01 in py and lil

--- 2023/01/202301.py
+++ 2023/01/202301.py
@@ -1,29 +1,13 @@
-digs = lambda s: [c for c in s if "0" <= c <= "9"]
-code = lambda ds: int(ds[0]+ds[-1])
+import re
+def ins(x): print(x); return x
 input = open("input.txt").read().splitlines()
-print("PART1:", sum(code(digs(line)) for line in input))
+print(sum(
+  int(g[0]+g[-1])
+  for line in input if (g := re.findall(r"([1-9])", line))))
 
 words = "one two three four five six seven eight nine".split()
-
-def fd(line):
-  for i in range(len(line)):
-    if "0" <= line[i] <= "9":
-      return int(line[i])
-    for n,w in enumerate(words):
-      if line.startswith(w, i):
-        return n + 1
-
-def ld(line):
- for i in range(len(line)-1,-1,-1):
-   if "0" <= line[i] <= "9":
-     return int(line[i])
-   for n,w in enumerate(words):
-     if line.startswith(w, i):
-       return n + 1
-
-def part2(input):
-  return sum(fd(line) * 10 + ld(line) for line in input)
-
-tinput = open("tinput2.txt").read().splitlines()
-assert (part2(tinput)) == 281
-print("PART2:", part2(input))
+vals = {**{d:int(d) for d in "123456789"}, **{w:i+1 for i,w in enumerate(words)}}
+rx = r"[1-9]|" + "|".join(words)
+print(sum(
+  int(vals[g[0]]*10+vals[g[-1]])
+  for line in input if (g := re.findall(rx, line))))
