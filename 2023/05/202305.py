e777f5c50299db0a94a740ff395a59acccf766d0
MikeBeller <bellermike@gmail.com>
2023-12-22 01:56:08 +0000

    coalesce

--- 2023/05/202305.py
+++ 2023/05/202305.py
@@ -20,7 +20,7 @@ def seed_to_loc(maps, seed):
 print(min(seed_to_loc(maps, s) for s in seeds))
 
 def range_partition(x, y):
-  "range split into before, in, after y"
+  "split range x into before, in, after y"
   fst = max(x.start, y.start)
   lst = min(x.stop, y.stop)
   return range(x.start, fst), range(fst,lst), range(lst, x.stop)
@@ -37,25 +37,30 @@ def coalesce(ranges):
   return new_ranges
 
 seed_ranges = [range(seeds[i],seeds[i]+seeds[i+1]) for i in range(0,len(seeds)-1, 2)]
-print(seed_ranges)
-print(coalesce(seed_ranges))
-
 
 assert set(seed_ranges) == set(coalesce(seed_ranges))
 seed_ranges = coalesce(seed_ranges)
-print(seed_ranges)
-# for map in maps:
-#   new_ranges = []
-#   for to,frm,n in map:
-#     for seed_range in seed_ranges:
-#       y = range(frm, frm+n)
-#       delta = to - frm
-#       before,during,after = range_partition(seed_range, y)
-#       new_during = range(during.start+delta, during.stop+delta)
-#       #print(seed_range, (to,frm,n), [before,new_during,after])
-#       new_ranges.extend(
-#         coalesce([before, new_during, after]))
-#   seed_ranges = coalesce(new_ranges)
 
-# print(seed_ranges)
-# print(min(r.start for r in seed_ranges))
+def apply_stage(map, ranges):
+  new_ranges = []
+  for to,frm,n in map:
+    y = range(frm, frm+n)
+    delta = to - frm
+    skipped_ranges = []
+    for r in ranges:
+      before, during, after = range_partition(r, y)
+      if len(during) > 0:
+        new_ranges.append(range(during.start+delta, during.stop+delta))
+      skipped_ranges.extend((before, after))
+    ranges = skipped_ranges
+  new_ranges.extend(skipped_ranges)
+  cranges = coalesce(new_ranges)
+  return cranges
+
+ranges = seed_ranges.copy()
+print(ranges)
+for map in maps:
+  ranges = apply_stage(map, ranges)
+  print(ranges)
+  
+print(min(r.start for r in ranges))
