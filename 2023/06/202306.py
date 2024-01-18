aefab200114572d1ce0e04782ffa0d7b3bda556d
MikeBeller <bellermike@gmail.com>
2024-01-18 22:40:15 +0000

    06 complete

--- 2023/06/202306.py
+++ 2023/06/202306.py
@@ -1,4 +1,4 @@
-from functools import reduce
+from math import sqrt, ceil, floor, prod
 
 def parse(s):
   ts,ds = s.splitlines()
@@ -9,20 +9,30 @@ def parse(s):
 tinput = parse(open("tinput.txt").read())
 input = parse(open("input.txt").read())
 
-def dt(t,n):
-  assert n > 0 and n < t
-  return n * (t - n)
+def solve(t,d):
+  sq = sqrt(t*t - 4 * d)
+  return (t - sq)/2, (t + sq)/2
 
 def ng(t,d):
-  return sum(1 for n in range(1,t-1)
-            if dt(t,n) > d)
+  a,b = solve(t,d)
+  ac = ceil(a) if a != floor(a) else ceil(a) + 1
+  bf = floor(b) if b != floor(b) else floor(b) - 1
+  return bf - ac + 1
 
 assert ng(7,9) == 4
 assert ng(15,40) == 8
 assert ng(30, 200) == 9
 
 def part1(inp):
-  return reduce(lambda a,x: a * ng(*x), inp, 1)
+  return prod(ng(*x) for x in inp)
 
 assert part1(tinput) == 288
 print(part1(input))
+
+def part2(inp):
+  T = float("".join(str(t) for t,d in inp))
+  D = float("".join(str(d) for t,d in inp))
+  return ng(T,D)
+
+assert part2(tinput) == 71503
+print(part2(input))
