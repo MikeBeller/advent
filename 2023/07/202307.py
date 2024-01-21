f0f43e23dde93605878b77f6f07e716b2ffdd597
MikeBeller <bellermike@gmail.com>
2024-01-21 02:13:49 +0000

    Part 2 not working

--- 2023/07/202307.py
+++ 2023/07/202307.py
@@ -12,12 +12,31 @@ def hand_score(code):
 
 assert hand_score("RR677") == (2, 2, 1)
   
-def part1(inp):
-  data = [(hand_score(code), code, score)
-          for code, score in inp]
+def run(inp, scorer):
+  data = [(scorer(code), code, weight)
+          for code, weight in inp]
   data.sort()
-  return sum((i+1) * score
-    for i,(hand,code,score) in enumerate(data))
+  return sum((i+1) * weight
+    for i,(hand,code,weight) in enumerate(data))
+
+def part1(inp):
+  return run(inp, hand_score)
 
 assert part1(tinput) == 6440
 print(part1(input))
+
+def hand_score_2(code):
+  cc = code.replace("J", "")
+  nj = 5 - len(cc)
+  if nj == 5:
+    return (5,)
+  else:
+    sc = list(hand_score(cc))
+    sc[0] += nj
+    return tuple(sc)
+
+def part2(inp):
+  return run(inp, hand_score_2)
+
+assert part2(tinput) == 5905
+print(part2(input))
