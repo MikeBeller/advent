77f1f16dfbaa82701482f5d85561e579543f9a32
MikeBeller <bellermike@gmail.com>
2024-02-06 11:23:16 +0000

    P2

--- 2023/08/202308.py
+++ 2023/08/202308.py
@@ -24,4 +24,25 @@ def part1(data):
 
 assert part1(tinput) == 2
 print(part1(input))
-    
+
+tinput3 = parse(open("tinput3.txt").read())
+
+def part2(data):
+	path,mp = data
+	ghosts = [s for s in mp.keys()
+	  if s.endswith("A")]
+	count = 0
+	steps = 0
+	rpath = repeat(path)
+	while count < len(ghosts):
+		count = 0
+		d = next(rpath)
+		for i,g in enumerate(ghosts):
+			ghosts[i] = mp[g][0] if d == "L" else mp[g][1]
+			if ghosts[i].endswith("Z"):
+				count += 1
+		steps += 1
+	return steps
+			
+assert part2(tinput3) == 6
+print(part2(input))
