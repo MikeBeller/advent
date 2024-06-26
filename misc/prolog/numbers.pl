0a4a3e150a5538aee69903deb51e27924262455d
MikeBeller <mike@beller.tech>
2024-06-26 16:15:53 +0000

    more

--- misc/prolog/numbers.pl
+++ misc/prolog/numbers.pl
@@ -2,24 +2,26 @@
 :- use_module(library(dcgs)).
 :- use_module(library(charsio)).
 
-
 read_integers(File, Integers) :-
     phrase_from_file(integer_list(Integers), File).
 
-integer_list([I|Is]) --> integer(I), blanks, integer_list(Is).
+integer_list([I|Is]) --> optional_blanks, integer(I), optional_blanks, integer_list(Is) optional_blanks.
 integer_list([]) --> [].
 
+integer(I) --> sign('+'), digits(Ds), { maplist(char_code, Ds, Cs), number_codes(I, Cs) } .
+integer(I) --> sign('-'), digits(Ds), { maplist(char_code, Ds, Cs), number_codes(I, [45 | Cs]) } .
 
-integer(I) --> minus_sign(N), digits(D), { number_codes(I, [N|D]) }.
-integer(I) --> digits(D), { number_codes(I, D) }.
-
-minus_sign('-') --> "-", !.
-minus_sign('') --> [].
+sign('-') --> "-", !.
+sign('+') --> "+", !.
 
 digits([D|Ds]) --> digit(D), digits(Ds).
-digits([]) --> [].
+digits([D]) --> [D].
 
 digit(D) --> [D], { char_type(D, decimal_digit) }.
 
+optional_blanks --> [C], { char_type(C, whitespace) }, optional_blanks.
+optional_blanks --> [].
+
 blanks --> [C], { char_type(C, whitespace) }, blanks.
-blanks --> [].
+blanks --> [C], { char_type(C, whitespace) }.
+
