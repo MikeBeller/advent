f0ce05f24cba88c7a3ea636a8720d617b6476bab
Mike Beller <mike@beller.tech>
2024-06-27 01:42:59 +0000

    fox hen poblem in python

--- misc/prolog/fox_hen.py
+++ misc/prolog/fox_hen.py
@@ -1,28 +1,34 @@
-from dataclasses import dataclass
-from enum import StrEnum
+INITIAL_STATE = dict(fox='home', hen='home', corn='home', farmer='home')
+GOAL_STATE = dict(fox='market', hen='market', corn='market', farmer='market')
 
-class Loc(StrEnum):
-    HOME = 'home'
-    CART = 'cart'
-    MARKET = 'market'
+def is_safe_state(s):
+    return not ((s['farmer'] != s['hen']) and
+        (s['fox'] == s['hen'] or s['hen'] == s['corn']))
 
-@dataclass
-class State:
-    fox: Loc
-    hen: Loc
-    corn: Loc
-    farmer: Loc
+assert not is_safe_state(dict(fox='home', hen='home', corn='market', farmer='market'))
+assert not is_safe_state(dict(fox='home', hen='market', corn='market', farmer='home'))
+assert is_safe_state(dict(fox='home', hen='market', corn='market', farmer='market'))
 
-INITIAL_STATE = State('home', 'home', 'home', 'home')
-GOAL_STATE = State('market', 'market', 'market', 'market')
+def move(s, other):
+    nxt = 'market' if s['farmer'] == 'home' else 'home'
+    s2 = dict(s, **{'farmer': nxt, other: nxt} )
+    assert is_safe_state(s2)
+    return s2
 
-def is_legal_state(s: State) -> bool:
-    # if the fox and the hen are together without the farmer -- opsie:
-    if s.fox == s.hen and not s.farmer = s.hen:
-        return False
-    elif s.hen == s.corn and not s.farmer = s.corn:
-        return False
-    else:
-        return True
+assert move(INITIAL_STATE, 'farmer') == dict(fox='home', hen='home', corn='home', farmer='market')
+assert move(INITIAL_STATE, 'hen') == dict(fox='home', hen='market', corn='home', farmer='market')
 
+def legal_moves(s):
+    # farmer can move by himself, or with any one other item in the same place as him:
+    return [next_state for other in ['fox', 'hen', 'corn', 'farmer']
+        if s['farmer'] == s[other] and
+            is_safe_state(next_state := move(s,other))]
 
+def solve(path):
+    if path[-1] == GOAL_STATE:
+        return path
+    for next_state in legal_moves(path[-1]):
+        return solve(moves + (next_state))
+
+result = solve((INITIAL_STATE,))
+print(result)
