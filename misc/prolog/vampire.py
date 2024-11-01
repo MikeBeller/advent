b02d872dcd3ccbf5153e8a38a549c05249de3614
Mike Beller <mike@beller.tech>
2024-10-31 21:13:18 -0400

    vampires

--- misc/prolog/vampire.py
+++ misc/prolog/vampire.py
@@ -1,3 +1,6 @@
+from itertools import permutations
+
+
 def dig(n: int) -> list[int]:
     return [int(d) for d in reversed(str(n))]
 
@@ -6,8 +9,30 @@ def num(n: list[int]) -> int:
     return sum(d * (10 ** i) for i, d in enumerate(n))
 
 
-def vampire(n: int) -> tuple[list[int], list[int]] | None:
+def vampire(n: int) -> tuple[int, int, int] | None:
     ds = dig(n)
     if len(ds) % 2 != 0:
         return False
     l2 = len(ds) // 2
+    for p in permutations(ds):
+        lf = p[:l2]
+        rt = p[l2:]
+        pr = num(lf) * num(rt)
+        if list(sorted(dig(pr))) == list(sorted(lf + rt)) and not (lf[0] == 0 and rt[0] == 0):
+            return pr, num(lf), num(rt)
+    return None
+
+
+def vampires(ndig: int) -> list[tuple[int, int, int]]:
+    vs = set()
+    for i in range(10**(ndig-1), 10**(ndig)):
+        if (v := vampire(i)):
+            pr, lf, rt = v
+            if lf > rt:
+                lf, rt = rt, lf
+            vs.add((pr, lf, rt))
+    return vs
+
+
+vs = vampires(6)
+print(len(vs), vs)
