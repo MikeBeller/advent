d8b7e4f6c419218f5e5ede070fe418634afc6019
MikeBeller <bellermike@gmail.com>
2024-12-09 16:57:41 +0000

    05 working both parts

--- 2024/05/202405.py
+++ 2024/05/202405.py
@@ -3,8 +3,8 @@ def parse(s):
   rss, uss = s.strip().split("\n\n")
   rules = defaultdict(set)
   for line in rss.splitlines():
-    xx,yy = line.strip().split("|")
-    rules[int(xx)].add(int(yy))
+    sx,sy = line.strip().split("|")
+    rules[int(sx)].add(int(sy))
   updates = [[int(x) for x in line.strip().split(",")]
               for line in uss.splitlines()]
   return rules, updates
@@ -30,3 +30,27 @@ trules, tupdates = parse(open("tinput.txt").read())
 assert part1(trules, tupdates) == 143
 rules, updates = parse(open("input.txt").read())
 print(part1(rules, updates))
+
+def fix(rules, update):
+  ok = True
+  ps = list(reversed(update))
+  for i in range(0,len(ps)-1):
+    p = ps[i]
+    for j in range(i+1,len(ps)):
+      p2 = ps[j]
+      if p2 in rules[p]:
+        ps[j], ps[i] = ps[i], ps[j]
+  return list(reversed(ps))
+
+def part2(rules, updates):
+  invalid_updates = [u for u in updates if not valid(rules, u)]
+  fixed = []
+  for update in invalid_updates:
+    u = update.copy()
+    while not valid(rules, u):
+      u = fix(rules, u)
+    fixed.append(u)
+  return sum( [u[len(u)//2] for u in fixed])
+  
+assert part2(trules, tupdates) == 123
+print(part2(rules, updates))
