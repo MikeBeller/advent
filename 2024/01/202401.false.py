f27850563d746443afae8b0b280d18a48ca19487
MikeBeller <bellermike@gmail.com>
2024-12-25 12:22:35 +0000

    Refactor

--- 2024/01/202401.false.py
+++ 2024/01/202401.false.py
@@ -4,15 +4,20 @@ sys.path.append('../../misc/false_lang')
 from false import FalseMachine
 
 code = r'''
-{ -- chr num -- input a number}
-[
-  0
-  [ ^ $$ '9 > \ '0 \> | ~]  {num dig isdig?}
-    [ '0 - \ 10 * + ] #
-\ ] d:
+{ c -- ? | is c a digit }
+[ $ '9 > \ '0 \> | ~]  d:
 
-d;! .
+{ -- c | get a char and also copy it to c:}
+[ ^ $ c: ] g:
 
+{ -- num -- input a number}
+[ 0 [ g;! d;!] [ c; '0 - \ 10 * + ] # ] n:
+
+n;!
+
+{ read numbers into array at address
+  pointed to by a, with len stored
+  in first cell }
 
 '''
 
