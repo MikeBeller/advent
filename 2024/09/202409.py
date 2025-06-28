d6331beeb1a5fbb5e13ccaf0b25bf34c6170af21
Mike Beller <mike@beller.tech>
2025-06-28 18:01:29 +0000

    09

--- 2024/09/202409.py
+++ 2024/09/202409.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 
 tinput = open("tinput.txt").read().strip()
 input = open("input.txt").read().strip()
@@ -37,10 +38,29 @@ assert part1(tinput) == 1928
 print(part1(input))
 
 def fill_full(inp: list[int]) -> list[int]:
-    r = inp[:]
-    return r
+    blocks = [list(g) for v,g in groupby(inp)]
+    print(blocks)
+    bi = len(blocks) - 1
+    while bi > 0:
+        b = blocks[bi]
+        l = len(b)
+        for si,fb in enumerate(blocks[:bi]):
+            if fb[0] != -1:
+                continue
+            if l > len(fb):
+                continue
+            blocks[si] = b
+            blocks.pop()
+            if l < len(fb):
+                blocks.insert(si, [-1]*(len(fb) - l))
+            break
+        bi -= 1
+
+
+    return inp
 
 def part2(inp: str) -> int:
     return checksum(fill_full(expand(inp)))
 
 print(part2(tinput))
+
