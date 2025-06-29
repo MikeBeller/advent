6dacf095e230fcd94f80e8b9709e9b5e592c6f46
MikeBeller <bellermike@gmail.com>
2025-06-29 18:08:51 +0000

    working on 09 in lua

--- 2024/09/202409.lua
+++ 2024/09/202409.lua
@@ -1,3 +1,21 @@
+local insert = table.insert
+
+local function dump(tbl, indent, visited)
+  indent = indent or ""; visited = visited or {}
+  if visited[tbl] then print(indent .. "*RECURSION*"); return end
+  visited[tbl] = true
+  for k, v in pairs(tbl) do
+    local key = tostring(k)
+    if type(v) == "table" then
+      print(indent .. key .. " = {")
+      dump(v, indent .. "  ", visited)
+      print(indent .. "}")
+    else
+      print(indent .. key .. " = " .. tostring(v))
+    end
+  end
+end
+
 local tinput = io.open("tinput.txt"):read("*a")
 local input = io.open("input.txt"):read("*a")
 
@@ -19,11 +37,27 @@ end
 
 local function blocks(xs)
   local r = {}
+  local v = xs[1]
+  local bs = 1
+  for i = 1,#xs do
+    if xs[i] ~= v then
+      r[#r + 1] = {bs=bs, be=i-1, v=v}
+      bs = i
+      v = xs[i]
+    end
+  end
+  if bs ~= #xs then
+    r[#r + 1] = {bs=bs, be=#xs, v=v}
+  end
+  --dump(r)
+  return r
 end
 
 local function part1(inp)
   local r = expand(inp)
-  print(r)
+  local bs = blocks(r)
+  
+  --dump(r)
 end
 
 part1(tinput)
