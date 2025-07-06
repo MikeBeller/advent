cbadab0f04bf5ea86a4a1b226b262a6ace123150
MikeBeller <bellermike@gmail.com>
2025-07-06 00:33:59 +0000

    10 working in python

--- 2024/10/202410.py
+++ 2024/10/202410.py
@@ -1,11 +1,12 @@
 from dataclasses import dataclass
+from collections import defaultdict
 
 def p(x):
   print(x)
   return x
 
 Point = tuple[int,int]
-U,R,D,L = 0,1,2,3
+Path = frozenset[Point]
 
 class Map:
   def __init__(self, s: str):
@@ -16,46 +17,63 @@ class Map:
             for r,line in enumerate(s.splitlines())
             for c,ch in enumerate(line)
       }
+    self.adj: dict[Point,set] = defaultdict(set)
+    self.heads = set()
+    self.tails = set()
+    for r in range(self.nr):
+      for c in range(self.nc):
+        p = (c,r)
+        if self.data[p] == 0:
+          self.heads.add(p)
+        elif self.data[p] == 9:
+          self.tails.add(p)
 
-  def valid(self, p: Point) -> bool:
-    (c,r) = p
-    return c >= 0 and c < self.nc and r >=0 and r < self.nr
+        for dr,dc in [(-1,0),(1,0),(0,-1),(0,1)]:
+          np = (c + dc,r + dr)
+          if np in self.data:
+            if self.data[np] - self.data[p] == 1:
+              self.adj[p].add(np)
 
   def __getitem__(self, key):
     return self.data[key]
 
 input = Map(open("input.txt").read())
 tinput = Map(open("tinput.txt").read())
-
-def move(p: Point, d: int):
-  (c,r) = p
-  if d == U:
-    return (c,r-1)
-  elif d == R:
-    return (c+1,r)
-  elif d == D:
-    return (c,r+1)
-  else: # L
-    return (c-1,r)
     
-def find_paths(mp: Map, st: Point, path: set(Point)=frozenset(), paths: set(set(Point))=frozenset()) -> frozenset:
-  if mp[st] == 9:
+def find_paths(mp: Map,
+               p: Point,
+               path: Path=Path(),
+               paths: frozenset[Path]=frozenset()) -> frozenset[Path]:
+  path = path | frozenset([p])
+  if mp[p] == 9:
     return paths | frozenset([path])
-  moves = [(c,r) for (c,r) in [move(st,d) for d in [U,R,D,L]]
-           if mp.valid((c,r)) and (c,r) not in path]
   return paths.union(
-    *[find_paths(mp, mv, (path | frozenset([st])), paths)
-             for mv in moves
-             if mp[mv] - mp[st] == 1])
+    *[find_paths(mp, mv, path, paths)
+             for mv in mp.adj[p]]
+  )
 
 def part1(mp: Map) -> int:
-  return sum(
-    p(len(find_paths(mp,(c,r))))
-      for c in range(mp.nc)
-        for r in range(mp.nr)
-          if mp[(c,r)] == 0)
+  #print(mp.tails)
+  s = 0
+  for h in mp.heads:
+    r:frozenset[Point] = frozenset()
+    ps = find_paths(mp,h)
+    for p in ps:
+      t = (p & mp.tails)
+      r = r | t
+    s += len(r)
+  return s
 
+assert part1(tinput) == 36
+print(part1(input))
 
-# #assert part1(tinput) == 36
-print(part1(tinput))
+def part2(mp: Map) -> int:
+  s = 0
+  for h in mp.heads:
+    ps = find_paths(mp,h)
+    #print(h, len(ps))
+    s += len(ps)
+  return s
 
+assert part2(tinput) == 81
+print(part2(input))
