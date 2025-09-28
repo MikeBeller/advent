f71f16baf2055adc18d49fea1d3120af9595e2bf
mike beller <mike@beller.tech>
2025-09-28 17:45:37 -0400

    working C for 1803 part 1 in CPM

---
+++ misc/cpm/T1803.C
@@ -0,0 +1,105 @@
+#include "stdio.h"
+
+char buf[128];
+
+struct poly {
+    short x1;
+    short y1;
+    short x2;
+    short y2;
+};
+
+struct poly polys[1500];
+unsigned int num_polys;
+unsigned char grid[10][1000];
+
+int min(x,y)
+  unsigned int x,y;
+{
+    return ((x < y) ? x : y);
+}
+
+int max(x,y) 
+  unsigned int x,y;
+{
+    return ((x > y) ? x : y);
+}
+
+void zero_grid() {
+    int x,y;
+    for (y = 0; y < 10; y++) {
+        for (x = 0; x < 1000; x++) {
+            grid[y][x] = 0;
+        }
+    }
+}
+
+void blit_grid(ymin, x1, y1, x2, y2)
+  unsigned int ymin, x1, y1, x2, y2;
+ {
+    unsigned int x, y;
+    for (y = y1; y < y2; y++) {
+        for (x = x1; x < x2; x++) {
+            grid[y-ymin][x] = grid[y-ymin][x] + 1;
+        }
+    }     
+}
+
+unsigned count_overlap(ymin) 
+    unsigned int ymin;
+{
+    unsigned int ymax, ox1, oy1, ox2, oy2;
+    unsigned int i, x, y;
+    unsigned long count;
+    ymax = ymin + 10;
+    zero_grid();
+    for (i = 0; i < num_polys; i++) {
+        ox1 = polys[i].x1;
+        ox2 = polys[i].x2;
+        oy1 = max(ymin, polys[i].y1);
+        oy2 = min(ymax, polys[i].y2);
+        if (oy2 > oy1) {
+            blit_grid(ymin, ox1, oy1, ox2, oy2);
+        }
+    }
+
+    count = 0;
+    for (y = 0; y < 10; y++) {
+        for (x = 0; x < 1000; x++) {
+            if ((grid[y][x] > 1) ? 1 : 0) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    FILE *fp;
+    unsigned short x, y, w, h;
+    unsigned int n, ymin;
+    unsigned long total_overlap;
+
+    if ((fp = fopen("T1803.TXT", "r")) == NULL) {
+        printf("Can't open file");
+        exit(1);
+    }
+
+    num_polys = 0;
+    while (!feof(fp)) {
+        fscanf(fp, "#%*d @ %d,%d: %dx%d\n", &x, &y, &w, &h);
+        polys[num_polys].x1 = x;
+        polys[num_polys].y1 = y;
+        polys[num_polys].x2 = x + w;
+        polys[num_polys].y2 = y + h; 
+        num_polys++;
+    }
+    printf("Num polys: %d\n", num_polys);
+
+    total_overlap = 0;
+    for (ymin = 0; ymin < 1000; ymin += 10) {
+        printf("TOP %d %ld\n", ymin, total_overlap);
+        total_overlap += count_overlap(ymin);
+    }
+    printf("%ld\n", total_overlap);
+}
