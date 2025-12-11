d9434973698664376631827133828627fb9f18f3
mike beller <mike@beller.tech>
2025-12-11 17:28:28 -0500

    06

--- 2025/06.py
+++ 2025/06.py
@@ -1,8 +1,9 @@
 from a import *
-lns = Lines(Rd("06.txt"))
-nr = len(lns) - 1
+lns = open("06.txt").read().splitlines()
 
 def part1(lns):
+    nums = []
+    nr = len(lns) - 1
     ns = [[int(s) for s in l.strip().split()]
             for l in lns[:nr]]
     ops = lns[-1].split()
@@ -13,13 +14,31 @@ def part1(lns):
         tot += sum(nums) if ops[i] == "+" else Prod(nums)
     print(tot)
 
+part1(lns)
+
 def part2(lns):
-    dlns = lns[:nr-1]
+    nr = len(lns) - 1
     ops = lns[-1]
     nc = len(lns[0])
     c = nc - 1
-    nums = []
+    tot = 0
     while c > 0:
-        num = 0
+        nums = []
+        while True:
+            num = 0
+            for r in range(nr):
+                ch = lns[r][c]
+                if ch != ' ':
+                    num = (num * 10) + int(ch)
+            nums.append(num)
+            if (op := ops[c]) != ' ':
+                tot += sum(nums) if op == '+' else Prod(nums)
+                nums = []
+                c -= 2
+                break
+            c -= 1
+    print(tot)
+
+part2(lns)
 
 
