f332e5785779c12c9d44b95fecb5fcb7e7c2298a
mike beller <mike@beller.tech>
2026-01-17 12:11:58 -0500

    01 in perl

--- 2025/01.py
+++ 2025/01.py
@@ -1,8 +1,15 @@
 from a import *
 
 inp = [ {'R': 1, 'L': -1}[s[0]]*int(s[1:]) for s in Lines(Rd("01.txt"))]
-
 print(Count(Zero, Acc(lambda a,b: (a + b) % 100, inp, 50)))
+
+# s = 50
+# c = 0
+# for line in open("01.txt").read().splitlines():
+#     n = int(line[1:]) if line[0] == "R" else -int(line[1:])
+#     s = (s + n) % 100
+#     c += s == 0
+# print(c)
 
 def part2(inp):
     tot = 50; c = 0
